05212d9afb2d738c7450d74ed8ad7e1292910e4c
hyzboy <devnull@localhost>
2014-07-11 13:13:43 +0800

    AngelVM中Map改为Bind

--- inc/hgl/script/AngelVM.h
+++ inc/hgl/script/AngelVM.h
@@ -1,4 +1,4 @@
-#ifndef HGL_ANGELVM_INCLUDE
+﻿#ifndef HGL_ANGELVM_INCLUDE
 #define HGL_ANGELVM_INCLUDE
 
 #include<hgl/script/VM.h>
@@ -121,9 +121,9 @@ namespace hgl
 			obj_name=n;
 		}
 
-		bool MapProperty(const char *,int);
-		bool MapArray(const char *,const char *,void *get=nullptr,void *set=nullptr);
-		bool MapFunc(const char *,void *);
+		bool BindProperty(const char *,int);
+		bool BindArray(const char *,const char *,void *get=nullptr,void *set=nullptr);
+		bool BindFunc(const char *,void *);
 	};//class AngelObject
 
 #define DEF_ANGEL_OBJECT(script_name,name)	static void Construct##script_name(name *this_pointer)	\
@@ -164,8 +164,8 @@ namespace hgl
 
 		static bool IsSupportMultiThread();															///<是否支持多线程
 
-		bool MapProperty(const char *,void *);														///<映射一个属性
-		bool MapFunc(const char *,void *);															///<映射一个函数
+		bool BindProperty(const char *,void *);														///<映射一个属性
+		bool BindFunc(const char *,void *);															///<映射一个函数
 
 		AngelObject *	CreateObject(const char *,int,void *construct,void *destruct);				///<映射一个对象
 
