f5313fb0139bbf2209909f4dbc657d53c13bb1f2
hyzboy <devnull@localhost>
2014-07-29 17:34:36 +0800

    增加使用范例

--- inc/hgl/pb/mp.h
+++ inc/hgl/pb/mp.h
@@ -97,7 +97,7 @@ namespace hgl
 			}
 
 			pb_item.ml->Clear();
-			
+
 			if(!pb_item.ml->ParseFromArray(data,size))
 			{
 				LOG_ERROR(OS_TEXT("Msg Parse error,MsgID:")+OSString(id));
@@ -107,5 +107,66 @@ namespace hgl
 			return pb_item.func(pb_item.ml);
 		}
 	};//class PBMessageProc
+
+	//使用范例
+/*	class UserThread
+	{
+	public:
+
+		UserThread()
+		{
+			mp.Init(c2s_login::id,c2s_request_all_level_info::id);								//初始化消息号
+
+		#define SET_PB_EVENT(name)	{	\
+										PBCmdFunc pcf;	\
+										\
+										SetEventCall(pcf,this,UserThread,on_##name);	\
+										\
+										mp.SetProc(c2s_##name::id,pcf,new c2s_##name);	\
+									}
+
+			SET_PB_EVENT(login);
+			SET_PB_EVENT(join_game);
+			SET_PB_EVENT(leave_table);
+
+			SET_PB_EVENT(showhand);
+			SET_PB_EVENT(giveup);
+			SET_PB_EVENT(follow);
+			SET_PB_EVENT(pass);
+
+			SET_PB_EVENT(showcard);
+
+			SET_PB_EVENT(request_roomlist);
+			SET_PB_EVENT(request_roominfo);
+			SET_PB_EVENT(requres_observe);
+			SET_PB_EVENT(bet_someone);
+			SET_PB_EVENT(request_next_game);
+
+			SET_PB_EVENT(request_all_level_info);
+
+		#undef SET_PB_EVENT
+		}
+
+		bool Update()
+		{
+			int32 msg_id;
+			int32 msg_size;
+
+			MemBlock<char> msg_data;
+
+			if(!dis->ReadInt32(msg_id))RETURN_FALSE;
+			if(!dis->ReadInt32(msg_size))RETURN_FALSE;
+
+			if(msg_size<0)RETURN_FALSE;
+			if(msg_size>0)
+			{
+				msg_data.SetLength(msg_size);
+				if(dis->ReadFully(msg_data.data(),msg_size)!=msg_size)
+					RETURN_FALSE;
+			}
+
+			return mp.Proc(msg_id,msg_data.data(),msg_size);
+		}
+	}*/
 }//namespace hgl
 #endif//HGL_PROTOCOLBUF_MESSAGE_PROCESS_CREATOR_INCLUDE
