4d122e3d4f13b9b332d77e4a01eccd8228108394
hyzboy <devnull@localhost>
2014-08-14 02:39:23 +0000

    修正APR版RWLock没有正确处理返回类型的问题

--- src/Base/MultiThread/APR/RWLockAPR.cpp
+++ src/Base/MultiThread/APR/RWLockAPR.cpp
@@ -17,11 +17,10 @@ namespace hgl
 	}
 	
 	bool RWLock::TryReadLock()	{ return apr_thread_rwlock_tryrdlock((apr_thread_rwlock_t *)lock) == APR_SUCCESS; }
-	void RWLock::ReadLock()		{ apr_thread_rwlock_rdlock((apr_thread_rwlock_t *)lock); }
-	void RWLock::ReadUnlock()	{ apr_thread_rwlock_unlock((apr_thread_rwlock_t *)lock); }
+	bool RWLock::ReadLock()		{ return apr_thread_rwlock_rdlock((apr_thread_rwlock_t *)lock) == APR_SUCCESS; }
+	bool RWLock::ReadUnlock()	{ return apr_thread_rwlock_unlock((apr_thread_rwlock_t *)lock) == APR_SUCCESS; }
 
 	bool RWLock::TryWriteLock()	{ return apr_thread_rwlock_trywrlock((apr_thread_rwlock_t *)lock) == APR_SUCCESS; }
-	void RWLock::WriteLock()	{ apr_thread_rwlock_wrlock((apr_thread_rwlock_t *)lock); }
-	void RWLock::WriteUnlock()	{ apr_thread_rwlock_unlock((apr_thread_rwlock_t *)lock); }
-
+	bool RWLock::WriteLock()	{ return apr_thread_rwlock_wrlock((apr_thread_rwlock_t *)lock) == APR_SUCCESS; }
+	bool RWLock::WriteUnlock()	{ return apr_thread_rwlock_unlock((apr_thread_rwlock_t *)lock) == APR_SUCCESS; }
 }//namespace hgl
