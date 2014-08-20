9ddcfb3e0a816476f69d4a21366a834dee6ca415
hyzboy <devnull@localhost>
2014-08-20 09:37:12 +0000

    修改PB收包处理

--- inc/hgl/pb/PBTCPSocketThread.h
+++ inc/hgl/pb/PBTCPSocketThread.h
@@ -47,10 +47,16 @@ namespace hgl
 				int32 msg_id;
 				int32 msg_size;
 
-				if(!this->dis->ReadInt32(msg_id))RETURN_FALSE;
 				if(!this->dis->ReadInt32(msg_size))RETURN_FALSE;
+				
+				if(msg_size==0)return(true);				//heart
+				
+				if(msg_size<sizeof(int32))RETURN_FALSE;
+				
+				if(!this->dis->ReadInt32(msg_id))RETURN_FALSE;
 
-				if(msg_size<0)RETURN_FALSE;
+				msg_size-=sizeof(int32);
+				
 				if(msg_size>0)
 				{
 					msg_data.SetLength(msg_size);
