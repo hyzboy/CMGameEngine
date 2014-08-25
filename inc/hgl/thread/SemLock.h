0a5af3a55a00a6136fc4821b4a45f0ab20d3950a
hyzboy <devnull@localhost>
2014-08-25 14:42:24 +0800

    SemLock系列重命名，避免冲突

--- inc/hgl/thread/SemLock.h
+++ inc/hgl/thread/SemLock.h
@@ -6,7 +6,7 @@
 #include<hgl/thread/RWLock.h>
 namespace hgl
 {
-	template<typename T,typename L> class SemLock
+	template<typename T,typename L> class SemLockTemplate
 	{
 	protected:
 
@@ -17,14 +17,14 @@ namespace hgl
 
 	public:
 
-		SemLock()HGL_DEFAULT_MEMFNUC;
+		SemLockTemplate(){}
 
 		/**
 		 * 本类构造函数
 		 * @param d 数据
 		 * @param sem_count 最大信号数量(默认0表示不限制)
 		 */
-		SemLock(T &d,int sem_count=0):sem(sem_count)
+		SemLockTemplate(T &d,int sem_count=0):sem(sem_count)
 		{
 			data=d;
 		}
@@ -54,16 +54,16 @@ namespace hgl
 		 * @return 是否成功
 		 */
 		bool Acquire(double time_out=0.0f){return sem.Acquire();}
-	};//template<typename T,typename L> class SemLock
+	};//template<typename T,typename L> class SemLockTemplate
 
 	/**
 	 * 信号线程排斥
 	 */
-	template<typename T> class SemThreadMutex:public SemLock<T,ThreadMutex>
+	template<typename T> class SemThreadMutex:public SemLockTemplate<T,ThreadMutex>
 	{
 	public:
 
-		using SemLock<T,ThreadMutex>::SemLock;
+		using SemLockTemplate<T,ThreadMutex>::SemLockTemplate;
 
 	public:	//线程排斥相关
 
@@ -102,10 +102,32 @@ namespace hgl
 		}
 
 		/**
+		 * 锁定
+		 * @param try_lock 是否是尝试锁定
+		 * @param time_out 如果不是尝试锁定，则等待锁定的超时时间
+		 */
+		bool SemLock(bool try_lock,double time_out)
+		{
+			if(try_lock)
+			{
+				if(!this->sem.TryAcquire())
+					return(false);
+			}
+			else				//当前没人，那就慢慢等吧
+			{
+				if(!this->sem.Acquire(time_out))
+					return(false);
+			}
+
+			this->lock.Lock();
+			return(true);
+		}
+
+		/**
 		 * 解锁并释放信号
 		 * @param n 释放的信号数量
 		 */
-		void	Unlock(int n=1)
+		void	SemUnlock(int n=1)
 		{
 			this->lock.Unlock();
 
@@ -117,15 +139,15 @@ namespace hgl
 	/**
 	 * 信号读写锁
 	 */
-	template<typename T> class SemRWLock:public SemLock<T,RWLock>
+	template<typename T> class SemRWLock:public SemLockTemplate<T,RWLock>
 	{
 	public:
 
-		using SemLock<T,RWLock>::SemLock;
+		using SemLockTemplate<T,RWLock>::SemLockTemplate;
 
 	public:	//读写锁相关
 
-		HGL_RWLock(this->lock);
+		HGL_RWLOCK(this->lock);
 
 	public:	//综合应用相关
 
@@ -134,7 +156,7 @@ namespace hgl
 		 * @param time_out 最长等待时间
 		 * @return 是否锁定了
 		 */
-		bool	WaitReadLock(double time_out)
+		bool	WaitSemReadLock(double time_out)
 		{
 			if(!this->sem.Acquire(time_out))
 				return(false);
@@ -148,7 +170,7 @@ namespace hgl
 		 * @param time_out 最长等待时间
 		 * @return 是否锁定了
 		 */
-		bool	WaitWriteLock(double time_out)
+		bool	WaitSemWriteLock(double time_out)
 		{
 			if(!this->sem.Acquire(time_out))
 				return(false);
@@ -161,7 +183,7 @@ namespace hgl
 		 * 尝试捕获一个信号，如果成功则共享锁定
 		 * @return 是否锁定了
 		 */
-		bool	TryReadLock()
+		bool	TrySemReadLock()
 		{
 			if(!this->sem.TryAcquire())
 				return(false);
@@ -174,7 +196,7 @@ namespace hgl
 		 * 尝试捕获一个信号，如果成功则独占锁定
 		 * @return 是否锁定了
 		 */
-		bool	TryWriteLock()
+		bool	TrySemWriteLock()
 		{
 			if(!this->sem.TryAcquire())
 				return(false);
@@ -187,7 +209,7 @@ namespace hgl
 		 * 解除共享锁并释放信号
 		 * @param n 释放的信号数量
 		 */
-		void	ReadUnlock(int n=1)
+		void	SemReadUnlock(int n=1)
 		{
 			this->lock.ReadUnlock();
 
@@ -199,7 +221,7 @@ namespace hgl
 		 * 解除独占锁并释放信号
 		 * @param n 释放的信号数量
 		 */
-		void	WriteUnlock(int n=1)
+		void	SemWriteUnlock(int n=1)
 		{
 			this->lock.WriteUnlock();
 
