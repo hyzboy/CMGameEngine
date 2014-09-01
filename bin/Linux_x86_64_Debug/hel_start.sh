dccd46925e30a84504d51b0ea4c7c6c7dd6f3c86
张超 <devnull@localhost>
2014-09-01 16:43:22 +0800

    哆啦A梦调试

---
+++ bin/Linux_x86_64_Debug/hel_start.sh
@@ -0,0 +1,1 @@
+valgrind --tool=helgrind --log-file=doraemonserver_helgrind.log --history-level=approx --error-limit=no --vgdb=yes ./doraemonserver
---
+++ bin/Linux_x86_64_Debug/memcheck_cs.sh
@@ -0,0 +1,1 @@
+valgrind --tool=memcheck --track-origins=yes --leak-check=full --show-reachable=yes --log-file=con_memcheck.log --vgdb=yes ./consortiumserver
