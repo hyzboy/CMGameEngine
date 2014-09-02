7440dffa576dc3e2dc941baa7eabe1e0c6ceb100
张超 <devnull@localhost>
2014-09-02 15:52:19 +0800

    恢复主分支数据

--- bin/Linux_x86_64_Debug/hel_start.sh
+++
@@ -1,1 +0,0 @@
-valgrind --tool=helgrind --log-file=doraemonserver_helgrind.log --history-level=approx --error-limit=no --vgdb=yes ./doraemonserver
--- bin/Linux_x86_64_Debug/table_convert.sh
+++
@@ -1,8 +0,0 @@
-﻿
-./csv2table CharaTable.txt
-./csv2table CharaLevel.txt
-./csv2table StoreTable.txt
-./csv2table HelpRole.txt
-./csv2table ItemTable.txt
-./csv2table MissionTable.txt
-./csv2table LevelTable.txt
