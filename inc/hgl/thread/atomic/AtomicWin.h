bf44a9554cfe79ae0c158b17a61142ccc4ca9395
hyzboy <devnull@localhost>
2014-09-20 07:02:39 +0000

    为Atomic Win增加强转换修饰，以防止C/C++自动重载时识别错函数

--- inc/hgl/thread/atomic/AtomicWin.h
+++ inc/hgl/thread/atomic/AtomicWin.h
@@ -34,15 +34,15 @@ namespace hgl
 					bool	operator == 		(const volatile T v)const	{return value==v;}
 					bool	operator != 		(const volatile T v)const	{return value!=v;}
 
-					T		operator =			(const volatile T nv)		{return InterlockedExchange((unsigned int *)&value,(unsigned int)nv);}
-					T		operator ++			()							{return InterlockedIncrement((unsigned int *)&value);}
-					T		operator --			()							{return InterlockedDecrement((unsigned int *)&value);}
-					T		operator +=			(const volatile T av)		{return InterlockedExchangeAdd(&value,av);}
-					T		operator -=			(const volatile T av)		{return InterlockedExchangeAdd(&value,-av);}
+					T		operator =			(const volatile T nv)		{return InterlockedExchange((unsigned long *)&value,(unsigned long)nv);}
+					T		operator ++			()							{return InterlockedIncrement((unsigned long *)&value);}
+					T		operator --			()							{return InterlockedDecrement((unsigned long *)&value);}
+					T		operator +=			(const volatile T av)		{return InterlockedExchangeAdd((unsigned long *)&value, av); }
+					T		operator -=			(const volatile T av)		{return InterlockedExchangeAdd((unsigned long *)&value, -av); }
 
-		volatile	T		operator &=			(const volatile T v)		{return InterlockedAnd(&value,v);}
-		volatile	T		operator |=			(const volatile T v)		{return InterlockedOr(&value,v);}
-		volatile	T		operator ^=			(const volatile T v)		{return InterlockedXor(&value,v);}
+		volatile	T		operator &=			(const volatile T v)		{return InterlockedAnd((unsigned long *)&value,v);}
+		volatile	T		operator |=			(const volatile T v)		{return InterlockedOr((unsigned long *)&value, v); }
+		volatile	T		operator ^=			(const volatile T v)		{return InterlockedXor((unsigned long *)&value, v); }
 
 		volatile	T		operator ++			(int)						{volatile T ret=value;operator++();return ret;}//后置++
 		volatile	T		operator --			(int)   					{volatile T ret=value;operator--();return ret;}//后置--
@@ -74,15 +74,15 @@ namespace hgl
 					bool	operator == 		(const volatile T v)const	{return value==v;}
 					bool	operator != 		(const volatile T v)const	{return value!=v;}
 
-					T		operator =			(const volatile T nv)		{return InterlockedExchange64((unsigned int *)&value,(unsigned int)nv);}
-					T		operator ++			()							{return InterlockedIncrement64((unsigned int *)&value);}
-					T		operator --			()							{return InterlockedDecrement64((unsigned int *)&value);}
-					T		operator +=			(const volatile T av)		{return InterlockedExchangeAdd64(&value,av);}
-					T		operator -=			(const volatile T av)		{return InterlockedExchangeAdd64(&value,-av);}
+					T		operator =			(const volatile T nv)		{return InterlockedExchange64((unsigned __int64 *)&value, (unsigned __int64)nv); }
+					T		operator ++			()							{return InterlockedIncrement64((unsigned __int64 *)&value); }
+					T		operator --			()							{return InterlockedDecrement64((unsigned __int64 *)&value); }
+					T		operator +=			(const volatile T av)		{return InterlockedExchangeAdd64((unsigned __int64 *)&value, av); }
+					T		operator -=			(const volatile T av)		{return InterlockedExchangeAdd64((unsigned __int64 *)&value, -av); }
 
-		volatile	T		operator &=			(const volatile T v)		{return InterlockedAnd64(&value,v);}
-		volatile	T		operator |=			(const volatile T v)		{return InterlockedOr64(&value,v);}
-		volatile	T		operator ^=			(const volatile T v)		{return InterlockedXor64(&value,v);}
+		volatile	T		operator &=			(const volatile T v)		{return InterlockedAnd64((unsigned __int64 *)&value, v); }
+		volatile	T		operator |=			(const volatile T v)		{return InterlockedOr64((unsigned __int64 *)&value, v); }
+		volatile	T		operator ^=			(const volatile T v)		{return InterlockedXor64((unsigned __int64 *)&value, v); }
 
 		volatile	T		operator ++			(int)						{volatile T ret=value;operator++();return ret;}//后置++
 		volatile	T		operator --			(int)   					{volatile T ret=value;operator--();return ret;}//后置--
