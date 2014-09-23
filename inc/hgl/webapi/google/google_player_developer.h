6c672189d7daa5f028b4e3d3ba2b02f226bae69b
hyzboy <devnull@localhost>
2014-09-23 11:16:03 +0800

    增加注释说明

--- inc/hgl/webapi/google/google_player_developer.h
+++ inc/hgl/webapi/google/google_player_developer.h
@@ -9,6 +9,14 @@ namespace hgl
 	{
 		namespace google
 		{
+			/**
+			 * 执行一个购买信息获取
+			 * @param packageName 软件包的名称，如“com.microcity.showhand”
+			 * @param productId 购买的物品ID，如“sku_money”
+			 * @param token Google返回的结帐标识
+			 * @return >0 购买成功
+			 * @return <0 失败错误码
+			 */
 			int PurchasesProductsGet(const UTF8String &packageName,const UTF8String &productId,const UTF8String &token);
 		}//namespace google
 	}//namespace webapi
