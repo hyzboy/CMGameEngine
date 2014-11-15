9ace2231b5cf1270e4d35666b19954fd9818bbf9
hyzboy <devnull@localhost>
2014-11-15 16:24:59 +0800

    增加RedisDBSaveThread类

---
+++ inc/hgl/db/RedisDBSaveThread.h
@@ -0,0 +1,96 @@
+﻿#ifndef HGL_REDIS_DB_SAVE_THREAD_INCLUDE
+#define HGL_REDIS_DB_SAVE_THREAD_INCLUDE
+
+#include<hgl/thread/Thread.h>
+#include<hgl/thread/CondVar.h>
+#include<hgl/db/RedisDB.h>
+
+namespace hgl
+{
+	namespace redis
+	{
+		/**
+		 * RedisDB保存数据专用线程<br>
+		 * 由于这里使用批量提交操作，所以仅可用于各种无需立即处理的数据保存
+		 */
+		class RedisDBSaveThread:public Thread
+		{
+			ThreadMutex tm;
+			CondVar cv;
+
+			RedisDB *rdb;
+
+			int count;
+			int max_count;
+			double time_out;
+
+		public:
+
+			RedisDBSaveThread()
+			{
+				rdb=nullptr;
+				count=0;
+			}
+
+			~RedisDBSaveThread()
+			{
+				SAFE_CLEAR(rdb);
+			}
+
+			/**
+			 * 初始化RedisDB
+			 * @param db RedisDB指针
+			 * @param mc 强制提交的数量
+			 * @param to 强制提交的时间
+			 */
+			void Init(RedisDB *db,const int mc=100,const double to=10)
+			{
+				rdb=db;
+
+				rdb->Multi();
+
+				count=0;
+				max_count=mc;
+				time_out=to;
+			}
+
+			RedisDB *Get()
+			{
+				tm.Lock();
+				return rdb;
+			}
+
+			RedisDB *TryGet()
+			{
+				if(!tm.TryLock())
+					return(nullptr);
+
+				return rdb;
+			}
+
+			void Release()
+			{
+				int result=++count;
+
+				tm.Unlock();
+
+				if(result>=max_count)		//强制提交一回
+					cv.Signal();
+			}
+
+			bool Execute()
+			{
+				if(!cv.Wait(&tm,time_out))
+					return(true);
+
+				rdb->Exec();
+				rdb->Multi();
+				count=0;
+
+				tm.Unlock();
+				return(true);
+			}
+		};//class RedisDBSaveThread
+	}//namespace redis
+}//namespace hgl
+#endif//HGL_REDIS_DB_SAVE_THREAD_INCLUDE
