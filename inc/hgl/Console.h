f8391abdd56b100ca00f39b0a992ffd72635f929
hyzboy <devnull@localhost>
2014-12-03 16:47:16 +0800

    ConsoleServer宏增加参数以增加初始化函数功能

--- inc/hgl/Console.h
+++ inc/hgl/Console.h
@@ -5,7 +5,7 @@
 #include<hgl/platform/ConsoleSystemInitInfo.h>
 #include<hgl/platform/ConsoleApplication.h>
 
-#define HGL_CONSOLE_MAIN(sii_name,app_name,arg_name)	extern "C" int ConsoleAppMain(hgl::ConsoleSystemInitInfo &sii_name,hgl::ConsoleApplication &app_name,const hgl::StringList<hgl::OSString> &arg_name)
+#define HGL_CONSOLE_MAIN(sii_name,app_name,arg_name)	extern "C" int ConsoleAppMain(hgl::ConsoleSystemInitInfo &sii_name,hgl::ConsoleApplication &app_name,const hgl::OSStringList &arg_name)
 
 #define HGL_CONSOLE_MAIN_FUNC()							using namespace hgl;    \
                                                         HGL_CONSOLE_MAIN(sii,app,args)
--- inc/hgl/ConsoleServer.h
+++ inc/hgl/ConsoleServer.h
@@ -7,24 +7,27 @@
 
 namespace hgl
 {
-	#define HGL_CONSOLE_SERVER_APPLICATION(name,code,server_class,thread_class,ip,port)	using namespace hgl;	\
-																						\
-																						HGL_CONSOLE_MAIN_FUNC()\
-																						{	\
-																							sii.info.ProjectName=OS_TEXT(name);	\
-																							sii.info.ProjectCode=OS_TEXT(code);	\
-																							\
-																							if(!app.Init(&sii))	\
-																								return(-1);	\
-																							\
-																							typedef SocketThreadServer<ConsoleFlowObject,server_class,thread_class> ConsoleStartServer;	\
-																							\
-																							app.SetStart(new ConsoleStartServer(ip,port));	\
-																							\
-																							return app.Run();	\
-																						}
+	#define HGL_CONSOLE_SERVER_APPLICATION(name,code,init_func,server_class,thread_class,ip,port)	using namespace hgl;	\
+																									\
+																									HGL_CONSOLE_MAIN_FUNC()\
+																									{	\
+																										sii.info.ProjectName=OS_TEXT(name);	\
+																										sii.info.ProjectCode=OS_TEXT(code);	\
+																										\
+																										if(!app.Init(&sii))	\
+																											return(-1);	\
+																										\
+																										typedef SocketThreadServer<ConsoleFlowObject,server_class,thread_class> ConsoleStartServer;	\
+																										\
+																										if(!init_func(args))	\
+																											return(-2);	\
+																										\
+																										app.SetStart(new ConsoleStartServer(ip,port));	\
+																										\
+																										return app.Run();	\
+																									}
 
-	#define HGL_CONSOLE_TCP_SERVER_APPLICATION(name,code,thread_class,ip,port)	HGL_CONSOLE_SERVER_APPLICATION(name,code,TCPServer,thread_class,ip,port)
-// 	#define HGL_CONSOLE_SCTP_SERVER_APPLICATION(name,code,thread_class,ip,port)	HGL_CONSOLE_SERVER_APPLICATION(name,code,SCTPServer,thread_class,ip,port)
+	#define HGL_CONSOLE_TCP_SERVER_APPLICATION(name,code,init_func,thread_class,ip,port)	HGL_CONSOLE_SERVER_APPLICATION(name,code,TCPServer,init_func,thread_class,ip,port)
+// 	#define HGL_CONSOLE_SCTP_SERVER_APPLICATION(name,code,init_func,thread_class,ip,port)	HGL_CONSOLE_SERVER_APPLICATION(name,code,SCTPServer,init_func,thread_class,ip,port)
 }//namespace hgl
 #endif//HGL_CONSOLE_SERVER_INCLUDE
