d90fcb1c0a76d3dc7ee910560e2929c3f4e5291b
hyzboy <>
2015-01-20 08:23:50 +0000

    增加ListenerOrientation的类型重定义，并增加支持Vector3f和ListenerOrientation的alGetListenerfv/alListenerfv函数，便于代码的编写

--- src/OpenALEE/Listener.cpp
+++ src/OpenALEE/Listener.cpp
@@ -1,29 +1,46 @@
-#include<hgl/audio/Listener.h>
+﻿#include<hgl/audio/Listener.h>
 #include<hgl/audio/OpenAL.h>
 #include<hgl/LogInfo.h>
 
 using namespace openal;
 namespace hgl
 {
+	inline void alGetListenerfv(ALenum param, Vector3f &v3f)
+	{
+		openal::alGetListenerfv(param, (ALfloat *)&v3f);
+	}
+	
+	inline void alGetListenerfv(ALenum param, ListenerOrientation &v3f)
+	{
+		openal::alGetListenerfv(param, (ALfloat *)&v3f);
+	}
+
+	inline void alListenerfv(ALenum param, const Vector3f &v3f)
+	{
+		openal::alListenerfv(param, (const ALfloat *)&v3f);
+	}
+
+	inline void alListenerfv(ALenum param, const ListenerOrientation &v3f)
+	{
+		openal::alListenerfv(param, (const ALfloat *)&v3f);
+	}
+
     AudioListener::AudioListener()
     {
 		if(!alGetListenerf)
 		{
-			LOG_ERROR(u"OpenAL/EE 还未初始化!");
+			LOG_ERROR(OS_TEXT("OpenAL/EE 还未初始化!"));
 			return;
 		}
 
-        orientation=new Vector3f[2];
-
         alGetListenerf(AL_GAIN,&gain);
-        alGetListenerfv(AL_POSITION,(ALfloat *)&position);
-        alGetListenerfv(AL_VELOCITY,(ALfloat *)&velocity);
-        alGetListenerfv(AL_ORIENTATION,(ALfloat *)&orientation);
+        alGetListenerfv(AL_POSITION,position);
+        alGetListenerfv(AL_VELOCITY,velocity);
+        alGetListenerfv(AL_ORIENTATION,orientation);
     }
 
     AudioListener::~AudioListener()
     {
-        delete[] orientation;
     }
 
     void AudioListener::SetGain(float _gain)
@@ -35,18 +52,18 @@ namespace hgl
 	void AudioListener::SetPosition(const Vector3f &pos)
     {
         position=pos;
-        alListenerfv(AL_POSITION,position.data());
+        alListenerfv(AL_POSITION,position);
     }
 
     void AudioListener::SetVelocity(const Vector3f &vel)
     {
         velocity=vel;
-        alListenerfv(AL_VELOCITY,velocity.data());
+        alListenerfv(AL_VELOCITY,velocity);
     }
 
-    void AudioListener::SetOrientation(const Vector3f *ori)
+    void AudioListener::SetOrientation(const ListenerOrientation &ori)
     {
-        memcpy(orientation,ori,2*sizeof(Vector3f));
-        alListenerfv(AL_ORIENTATION,(float *)orientation);
+        memcpy(orientation,ori,sizeof(ListenerOrientation));
+        alListenerfv(AL_ORIENTATION,orientation);
     }
 }//namespace hgl
