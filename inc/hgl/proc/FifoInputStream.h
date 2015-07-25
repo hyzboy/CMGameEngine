651b6967bd23e8e807705695671816fc04b41f1c
hyzboy <devnull@localhost>
2015-07-25 09:36:20 +0800

    增加KeyValue.h/PipeInputStream.h/FifoInputStream.h/dh.h头文件

---
+++ inc/hgl/proc/FifoInputStream.h
@@ -0,0 +1,35 @@
+﻿#ifndef HGL_MULTI_PROC_FIFO_INPUT_STREAM_INCLUDE
+#define HGL_MULTI_PROC_FIFO_INPUT_STREAM_INCLUDE
+
+#include<hgl/Fifo.h>
+#include<hgl/io/InputStream.h>
+
+namespace hgl
+{
+	namespace io
+	{
+		/**
+		 * 命名管道输入流
+		 */
+		class FifoInputStream:public InputStream
+		{
+			Fifo *f;
+
+		public:
+
+			FifoInputStream(Fifo *_f)
+			{
+				f=_f;
+			}
+
+			virtual ~FifoInputStream()
+			{
+				if(f)
+					delete f;
+			}
+
+			
+		};//class FifoInputStream
+	}//namespace io
+}//namespace hgl
+#endif//HGL_MULTI_PROC_FIFO_INPUT_STREAM_INCLUDE
---
+++ inc/hgl/ut/dh.h
@@ -0,0 +1,19 @@
+﻿#ifndef HGL_CRYPT_DH_INCLUDE
+#define HGL_CRYPT_DH_INCLUDE
+
+namespace hgl
+{
+	namespace util
+	{
+		/**
+		 * 密钥交换工具类
+		 */
+		class ECDH
+		{
+			
+		public:
+		};//class ECDH
+
+	}//namespace util
+}//namespace hgl
+#endif//HGL_CRYPT_DH_INCLUDE
