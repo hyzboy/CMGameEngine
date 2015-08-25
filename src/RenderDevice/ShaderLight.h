d67a66bbb170c031ede1cd5529a1f21a0588b726
hyzboy <>
2015-08-25 09:38:27 +0000

    增加glsl光照代码

---
+++ src/RenderDevice/ShaderLight.h
@@ -0,0 +1,18 @@
+#ifndef HGL_GRAPH_GEN_SHADER_LIGHT_INCLUDE
+#define HGL_GRAPH_GEN_SHADER_LIGHT_INCLUDE
+
+namespace hgl
+{
+	namespace graph
+	{
+		namespace shadergen
+		{
+			extern const char glsl_light_directional[];
+			extern const char glsl_light_point[];
+			extern const char glsl_light_spot[];
+			extern const char glsl_light_spot_direction[];
+		}//namespace shadergen
+	}//namespace graph
+}//namespace hgl
+
+#endif//HGL_GRAPH_GEN_SHADER_LIGHT_INCLUDE
