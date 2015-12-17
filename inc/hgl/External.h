09ddb8a2b3e9eeb5b4e30a48d4a14f26b5014252
hyzboy <devnull@localhost>
2015-12-17 15:38:58 +0400

    删除冗旧的自动链接.LIB文件宏

--- inc/hgl/External.h
+++ inc/hgl/External.h
@@ -3,9 +3,4 @@
 
 #include<hgl/external_core.h>
 
-#if !defined(__MAKE_HGL__) && !defined(__MAKE_PLUGIN__)
-	#pragma comment(lib,HGL_LIB_OpenALEE)		//连入OpenALEE音频库文件
-	#pragma comment(lib,HGL_LIB_Graphics)		//连入图形库文件
-	#pragma comment(lib,HGL_LIB_GUI)			//连入图形用户界面库文件
-#endif//!__MAKE_HGL__&&!__MAKE_PLUGIN__
 #endif//HGL_EXTERNAL_INCLUDE
--- inc/hgl/ExternalCore.h
+++ inc/hgl/ExternalCore.h
@@ -5,14 +5,6 @@
 #include<hgl/platform/ConsoleSystemInfo.h>
 #include<hgl.h>
 
-#if !defined(__MAKE_HGL__) && !defined(__MAKE_PLUGIN__)
-	#pragma comment(lib,HGL_LIB_Base)         	//连入基础库文件
-	#pragma comment(lib,HGL_LIB_APR)			//连入Apache Portable Runtime
-	#pragma comment(lib,HGL_LIB_Network)      	//连入网络库文件
-	#pragma comment(lib,HGL_LIB_UtilityTools) 	//连入实用工具库文件
-	#pragma comment(lib,HGL_LIB_WinExternal)	//连入标准Windows平台接口库文件
-#endif//!__MAKE_HGL__&&!__MAKE_PLUGIN__
-
 namespace hgl
 {
 	bool InitCore(ConsoleSystemInfo &si,bool create_log);	///<初始化核心
