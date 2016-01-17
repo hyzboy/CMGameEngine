55197423c1f5d39b10ce5b1fbe5ca0e3e8966d03
hyzboy <devnull@localhost>
2016-01-17 16:18:11 +0400

    拆分独立的TextureChannels.cpp

---
+++ src/RenderDevice/TextureChannels.cpp
@@ -0,0 +1,23 @@
+﻿#include<hgl/graph/TextureChannels.h>
+namespace hgl
+{
+    namespace graph
+    {
+        extern const char MaterialTextureName[mtcMax][32]=
+        {
+            "Diffuse",
+            "Ambient",
+            "Specular",
+            "Normal",
+            "Tangent",
+            "Light",
+            "Alpha",
+            "Shadow",
+
+            "Palette",
+
+            "Height",
+            "NormalHeight",
+        };
+    }//namespace graph
+}//namespace hgl
