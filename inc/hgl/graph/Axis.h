658e8513c635615d77f2ed57a1380fbe4b688911
hyzboy <devnull@localhost>
2016-01-17 23:24:09 +0400

    独立Axis枚举到Axis.h

---
+++ inc/hgl/graph/Axis.h
@@ -0,0 +1,23 @@
+﻿#ifndef HGL_GRAPH_AXIS_INCLUDE
+#define HGL_GRAPH_AXIS_INCLUDE
+namespace hgl
+{
+	namespace graph
+	{
+		/**
+		* 轴枚举
+		*/
+		enum Axis
+		{
+			HGL_AXIS_NONE=0,
+
+			HGL_AXIS_X,
+			HGL_AXIS_Y,
+			HGL_AXIS_Z,
+
+			HGL_AXIS_END
+
+		};//enum Axis
+	}//namespace graph
+}//namespace hgl
+#endif//HGL_GRAPH_AXIS_INCLUDE
