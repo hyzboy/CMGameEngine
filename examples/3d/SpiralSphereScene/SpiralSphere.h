b5532c36fd5f80be4f1097f8094e3a897f91221a
hyzboy <devnull@localhost>
2016-01-27 02:57:02 +0400

    增加SpiralSphereScene范例，用于渲染大量球体。为下一步增加光源测试做准备

---
+++ examples/3d/SpiralSphereScene/SpiralSphere.h
@@ -0,0 +1,42 @@
+﻿#ifndef HGL_EXAMPLE_3D_SPIRAL_SPHERE_INCLUDE
+#define HGL_EXAMPLE_3D_SPIRAL_SPHERE_INCLUDE
+
+#include<hgl/VectorMath.h>
+#include<hgl/graph/SceneNode.h>
+
+#define SPHERE_NUMBER	360
+
+namespace hgl
+{
+	namespace graph
+	{
+		class VertexArray;
+		class Material;
+		class Renderable;
+	}//namespace graph
+}//namespace hgl
+
+using namespace hgl;
+using namespace hgl::graph;
+
+class SpiralSphere
+{
+	VertexArray *sphere_data;
+	Material *sphere_mtl[SPHERE_NUMBER];
+
+	Renderable *sphere_obj[SPHERE_NUMBER];
+
+protected:
+
+	SceneNode node;					///<球体场景节点
+
+	RenderList sphere_rl;			///<球体场景渲染列表
+
+public:
+
+	SpiralSphere(const Vector3f &);
+	~SpiralSphere();
+
+	void Render(const Matrix4f *,const Matrix4f *);
+};//class SpiralSphere
+#endif//HGL_EXAMPLE_3D_SPIRAL_SPHERE_INCLUDE
