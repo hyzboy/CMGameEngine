7ce92e7a50016afcafd9816de85b69262cfc6562
hyzboy <devnull@localhost>
2016-06-23 18:02:41 +0800

    更新GLFW到3.2

--- src/RenderDevice/GLFW/glfw_config.h
+++ src/RenderDevice/GLFW/glfw_config.h
@@ -1,7 +1,7 @@
 //========================================================================
-// GLFW 3.1 - www.glfw.org
+// GLFW 3.2 - www.glfw.org
 //------------------------------------------------------------------------
-// Copyright (c) 2010 Camilla Berglund <elmindreda@elmindreda.org>
+// Copyright (c) 2010-2016 Camilla Berglund <elmindreda@glfw.org>
 //
 // This software is provided 'as-is', without any express or implied
 // warranty. In no event will the authors be held liable for any damages
@@ -35,9 +35,9 @@
 //========================================================================
 
 // Define this to 1 if building GLFW for X11
-/* #undef _GLFW_X11 */
+#define _GLFW_X11
 // Define this to 1 if building GLFW for Win32
-#define _GLFW_WIN32
+/* #undef _GLFW_WIN32 */
 // Define this to 1 if building GLFW for Cocoa
 /* #undef _GLFW_COCOA */
 // Define this to 1 if building GLFW for Wayland
@@ -45,25 +45,14 @@
 // Define this to 1 if building GLFW for Mir
 /* #undef _GLFW_MIR */
 
-// Define this to 1 if building GLFW for EGL
-/* #undef _GLFW_EGL */
-// Define this to 1 if building GLFW for GLX
-/* #undef _GLFW_GLX */
-// Define this to 1 if building GLFW for WGL
-#define _GLFW_WGL
-// Define this to 1 if building GLFW for NSGL
-/* #undef _GLFW_NSGL */
-
 // Define this to 1 if building as a shared library / dynamic library / DLL
-#define _GLFW_BUILD_DLL
+/* #undef _GLFW_BUILD_DLL */
 
 // Define this to 1 to force use of high-performance GPU on hybrid systems
 /* #undef _GLFW_USE_HYBRID_HPG */
 
-// Define this to 1 if the XInput X11 extension is available
-/* #undef _GLFW_HAS_XINPUT */
 // Define this to 1 if the Xxf86vm X11 extension is available
-/* #undef _GLFW_HAS_XF86VM */
+#define _GLFW_HAS_XF86VM
 
 // Define this to 1 if glfwInit should change the current directory
 /* #undef _GLFW_USE_CHDIR */
@@ -71,11 +60,4 @@
 /* #undef _GLFW_USE_MENUBAR */
 // Define this to 1 if windows should use full resolution on Retina displays
 /* #undef _GLFW_USE_RETINA */
-
-// Define this to 1 if using OpenGL as the client library
-#define _GLFW_USE_OPENGL
-// Define this to 1 if using OpenGL ES 1.1 as the client library
-/* #undef _GLFW_USE_GLESV1 */
-// Define this to 1 if using OpenGL ES 2.0 as the client library
-/* #undef _GLFW_USE_GLESV2 */
 
