ae342f7e9fbe981d7bcc8580e08cc4d55873a49d
hyzboy <devnull@localhost>
2016-06-25 10:04:37 +0800

    改写旧的注释

--- inc/hgl/object/FlowObject.h
+++ inc/hgl/object/FlowObject.h
@@ -7,11 +7,10 @@
 namespace hgl
 {
 	/**
-	* 流程对象是指可以放在流程控制器里的一种特殊对象，它类似于HGLv17和HGLv16加强版中的GameProgram。<br>
-	* 需要注意的是在HGLv18中，画面绘制和刷新是分离的，刷新是在不停的运行的，而画面绘制只在时间到达时才会被调用。<br>
-	* 另：KeyPreview默认为false,其为true时键盘事件先传给FlowObject的事件处理函数，为false时先传给Control内的控件
+	* 作业流程对象，负责提供刷新功能和绘制功能。供流程控制器进行调用，以完成整个作业。<br>
+	* 需要注意的是：画面绘制和刷新是分离的，刷新是在不停的运行的，而画面绘制只在时间到达时才会被调用。<br>
 	*/
-	class FlowObject:public EventObject                                         	                ///流程对象基类
+	class FlowObject:public EventObject                                         	                ///作业流程对象基类
 	{
 		friend class FlowControl;
 
@@ -30,7 +29,7 @@ namespace hgl
 
 	public: //属性
 
-		bool KeyPreview;                                                                            ///<键盘事件预览
+		bool KeyPreview;                                                                            ///<键盘事件预览(为ture时键盘事件优先自己处理，为false时优先传给Control中的控件。默认为false)
 
 		ObjectControl Control;                                                                      ///<对象控制器
 
@@ -47,10 +46,10 @@ namespace hgl
 		FlowObject();                                                                               ///<本类构造函数
 		virtual ~FlowObject();                                                                    	///<本类析构函数
 
-		virtual FlowObject *GetNextObject();                                                        ///<取得下一个对象
+		virtual FlowObject *GetNextObject();                                                        ///<取得下一个要执行的流程对象
 		virtual void ObjectReturn(FlowObject *);                                                  	///<对象返回函数
 
-		virtual void Update(){}																		///<刷新函数
+		virtual void Update(){}													                    ///<刷新函数
 		virtual void UpdateObject()
 		{
 			Update();
