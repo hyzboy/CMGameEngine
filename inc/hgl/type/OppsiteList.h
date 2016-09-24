3a31f6cd1e3a78f714a94a22436c3ff1c2d9846d
hyzboy <devnull@localhost>
2016-09-25 02:44:50 +0400

    OppsiteIndex改名为OppsiteList

---
+++ inc/hgl/type/OppsiteList.h
@@ -0,0 +1,14 @@
+﻿#ifndef HGL_TYPE_OPPSITE_LIST_INCLUDE
+#define HGL_TYPE_OPPSITE_LIST_INCLUDE
+
+namespace hgl
+{
+	/**
+	 * 相对列表，两组数据互为索引。用于需要经常相对查找的数据组合
+	 */
+	template<typename T,typename F> class OppsiteList
+	{
+	public:
+	};//template<typename T,typename F> class OppsiteList
+}//namespace hgl
+#endif//HGL_TYPE_OPPSITE_LIST_INCLUDE
