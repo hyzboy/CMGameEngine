a1e9fd03d694d5fb152700e06ae442f1f835d1f8
hyzboy <devnull@localhost>
2016-10-19 23:31:58 +0400

    LogFile.cpp操作系统相关部分迁移至CM.Platform，并将文件输出字符集在Win下设为utf16le,其它平台为utf8

---
+++ src/Platform/UNIX/LogFile.cpp
@@ -0,0 +1,33 @@
+﻿#include<hgl/io/TextOutputStream.h>
+#include<pwd.h>
+#include<unistd.h>
+
+namespace hgl
+{
+	namespace logger
+	{
+        void GetLocalAppdataPath(os_char fn[HGL_MAX_PATH])
+        {
+            struct passwd pwd;
+            struct passwd *result;
+            char *buf;
+            size_t bufsize=sysconf(_SC_GETPW_R_SIZE_MAX);
+
+            if(bufsize==-1)
+                bufsize=16384;
+
+            buf=(char *)malloc(bufsize);
+
+            getpwuid_r(getuid(),&pwd,buf,bufsize,&result);
+
+            strcpy(fn,HGL_MAX_PATH,pwd.pw_dir);
+
+            free(buf);
+        }
+
+        io::TextOutputStream *CreateTextOutputStream(io::OutputStream *os)
+        {
+            return(new io::UTF8TextOutputStream(os));
+        }
+	}//namespace logger
+}//namespace hgl
