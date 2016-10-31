396c147b4c50bcafd628196ad3e9904660c47829
hyzboy <devnull@localhost>
2016-10-31 23:37:49 +0400

    改进EventBase，为鼠标和UI事件做特殊处理

---
+++ inc/hgl/object/Object.ProcEvent.h
@@ -0,0 +1,10 @@
+﻿public:
+
+    virtual bool ProcMouseMove      (int,int);                                      ///<鼠标移动事件
+    virtual bool ProcMouseButton    (int,bool);                                     ///<鼠标按键事件
+    virtual bool ProcMouseWheel     (int,int);                                      ///<鼠标滚轮事件
+    virtual bool ProcJoystickButton (int,bool);                                     ///<游戏杆按键事件
+    virtual bool ProcKey            (int,bool);                                     ///<键盘按键事件
+    virtual bool ProcChar           (os_char);                                      ///<字符事件
+    virtual bool ProcTouch          (int,Vertex2i *);                               ///<触控点事件
+    virtual bool ProcEvent          (int,void *);                                   ///<自定义事件
--- inc/hgl/platform/EventBase.h
+++ inc/hgl/platform/EventBase.h
@@ -23,6 +23,11 @@ namespace hgl
 
             return OnProc(args...);
         }
+
+        virtual void Clear()
+        {
+            OnProc.ClearFunc();
+        }
     };//template<typename ...ARGS> class EventBaseCB
 
     /**
@@ -35,6 +40,11 @@ namespace hgl
         Set<EventBase *> sub_list;
 
     public:
+
+        virtual void Clear()
+        {
+            sub_list.Clear();
+        }
 
         virtual bool Proc(ARGS...args)
         {
@@ -70,6 +80,84 @@ namespace hgl
         }
     };//template<typename ...ARGS> class EventBase
 
+    /**
+     * 鼠标移动事件<br>
+     * 鼠标移动事件，每个控件都会传递，所以特殊处理
+     */
+    class EventMouseMove:public EventBase<const Vertex2i &>
+    {
+    public:
+
+        virtual bool Proc(const Vertex2i &pos) override
+        {
+            const int count=this->sub_list.GetCount();
+
+            if(count<=0)return(false);
+
+            bool result=false;
+
+            EventBase **ev=this->sub_list.GetData();
+
+            for(int i=0;i<count;i++)
+            {
+                if((*ev)->Proc(pos))
+                    result=true;
+
+                ++ev;
+            }
+
+            if(this->EventBase<const Vertex2i &>::Proc(pos))
+                result=true;
+
+            return result;
+        }
+    };//class EventMouseMove
+
+    /**
+     * UI事件<br>
+     */
+    template<typename ...ARGS> class EventUI:public EventBase<ARGS...>
+    {
+    public:
+
+        bool KeyPreview=false;
+
+        virtual bool Proc(ARGS...args) override
+        {
+            const int count=this->sub_list.GetCount();
+
+            if(count<=0)return(false);
+
+            if(KeyPreview)
+            {
+                if(this->EventBase<ARGS...>::Proc(args...))
+                    return(true);
+
+                EventBase<ARGS...> **ev=this->sub_list.GetData();
+
+                for(int i=0;i<count;i++)
+                {
+                    if((*ev)->Proc(args...))return(true);
+                    ++ev;
+                }
+
+                return(false);
+            }
+            else
+            {
+                EventBase<ARGS...> **ev=this->sub_list.GetData();
+
+                for(int i=0;i<count;i++)
+                {
+                    if((*ev)->Proc(args...))return(true);
+                    ++ev;
+                }
+
+                return this->EventBase<ARGS...>::Proc(args...);
+            }
+        }
+    };//class EventUI
+
     #define HGL_EV_SUB_JOIN(name)   this->name.Join(&(ev->name));
     #define HGL_EV_SUB_UNJOIN(name) this->name.Unjoin(&(ev->name));
 
@@ -80,16 +168,39 @@ namespace hgl
     {
     public:
 
-        EventBase<int,int>          MouseMove;                                  ///<鼠标移动事件
-        EventBase<int,bool>         MouseButton;                                ///<鼠标按键事件
-        EventBase<int,int>          MouseWheel;                                 ///<鼠标滚轮事件
-        EventBase<int,bool>         JoystickButton;                             ///<游戏杆按键事件
-        EventBase<int,bool>         Key;                                        ///<键盘按键事件
-        EventBase<os_char>          Char;                                       ///<字符事件
-        EventBase<int,Vertex2i *>   Touch;                                      ///<触控点事件
-        EventBase<int,void *,int>   Event;                                      ///<自定义事件
+        EventMouseMove              MouseMove;                                  ///<鼠标移动事件
+        EventUI<int,bool>           MouseButton;                                ///<鼠标按键事件
+        EventUI<int,int>            MouseWheel;                                 ///<鼠标滚轮事件
+        EventUI<int,bool>           JoystickButton;                             ///<游戏杆按键事件
+        EventUI<int,bool>           Key;                                        ///<键盘按键事件
+        EventUI<os_char>            Char;                                       ///<字符事件
+        EventUI<int,Vertex2i *>     Touch;                                      ///<触控点事件
+        EventUI<int,void *>         Event;                                      ///<自定义事件
 
     public:
+
+        virtual void SetKeyPreview(bool kp)
+        {
+            MouseButton     .KeyPreview=kp;
+            MouseWheel      .KeyPreview=kp;
+            JoystickButton  .KeyPreview=kp;
+            Key             .KeyPreview=kp;
+            Char            .KeyPreview=kp;
+            Touch           .KeyPreview=kp;
+            Event           .KeyPreview=kp;
+        }
+
+        virtual void Clear()
+        {
+            MouseMove.Clear();
+            MouseButton.Clear();
+            MouseWheel.Clear();
+            JoystickButton.Clear();
+            Key.Clear();
+            Char.Clear();
+            Touch.Clear();
+            Event.Clear();
+        }
 
         virtual void Join(RootEventBase *ev)
         {
@@ -127,13 +238,28 @@ namespace hgl
     {
     public:
 
+        EventBaseCB<int,int>    Resize;                     ///<窗口尺寸变动
         EventBaseCB<>           Close;                      ///<应用关闭
+
         EventBaseCB<int>        EnterBack;                  ///<进入后台
         EventBaseCB<int>        LeaveBack;                  ///<离开后台
         EventBaseCB<int>        EnterSleep;                 ///<进入休眠状态
         EventBaseCB<int>        LeaveSleep;                 ///<离开休眠状态
 
-        EventBaseCB<int,int>    Resize;                     ///<窗口尺寸变动
+    public:
+
+        virtual void Clear()
+        {
+            this->RootEventBase::Clear();
+
+            Resize.Clear();
+            Close.Clear();
+
+            EnterBack.Clear();
+            LeaveBack.Clear();
+            EnterSleep.Clear();
+            LeaveSleep.Clear();
+        }
     };//class AppEventBase:public RootEventBase
 
     /**
@@ -151,6 +277,18 @@ namespace hgl
         EventBase<>         LoseFocus;                      ///<控件失去焦点
 
     public:
+
+        virtual void Clear()
+        {
+            this->RootEventBase::Clear();
+
+            MouseEnter.Clear();
+            MouseLeave.Clear();
+            MouseDoubleClick.Clear();
+
+            GetFocus.Clear();
+            LoseFocus.Clear();
+        }
 
         virtual void Join(UIEventBase *ev)
         {
