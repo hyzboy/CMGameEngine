36d5103fd06e580cfcbdd12b2af3a2e7082a6603
hyzboy <devnull@localhost>
2016-11-01 16:31:52 +0400

    新的2D UI材质用glsl，此分支用于建立新的JSON配置文件材质系统。不再使用自适应SHADER生成器

---
+++ shader/glcore/2d_ui_rgb_alphatest.fs.glsl
@@ -0,0 +1,17 @@
+﻿#version 150 core
+
+uniform sampler2D Diffuse;
+in vec2 FragmentTexCoord_Diffuse;
+
+layout(location=0,index=0) out vec4 FragColor;
+
+void main(void)
+{
+	vec4 DiffuseColor=texture(Diffuse,FragmentTexCoord_Diffuse);
+
+	if(DiffuseColor.a<1)
+        discard;
+
+	FragColor=DiffuseColor;
+}
+
