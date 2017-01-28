fe8cbf085f778370e8fdacfb35f277961df9f4cd
hyzboy <devnull@localhost>
2017-01-28 22:44:46 +0800

    增加OPENGL API分级支持定义

---
+++ src/RenderDevice/API/apiTexture.cpp
@@ -0,0 +1,22 @@
+#include"apiTexture.h"
+
+namespace hgl
+{
+	namespace graph
+	{
+		namespace api
+		{
+			// 1.Bind(OpenGL 1.0)
+			// 2.Bind+Storage(4.2)
+			// 3.DSA+Storage(4.5)
+
+			/**
+			* ≥ı ºªØŒ∆¿ÌAPI 
+			**/
+			bool InitTextureAPI()
+			{
+
+			}
+		}//namespace api
+	}//namespace graph
+}//namespace hgl
