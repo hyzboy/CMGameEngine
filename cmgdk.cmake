52ad487a8906423b3814a67edb97214dafb3be7f
hyzboy <devnull@localhost>
2017-01-29 01:05:06 +0800

    Loginfo相关选项独立成loginfo.cmake

--- cmgdk.cmake
+++ cmgdk.cmake
@@ -14,47 +14,7 @@ OPTION(BUILD_TEST_PROJECT		"Build Test Project"						FALSE	)
 OPTION(BUILD_GUI_TOOLS			"Build GUI Tools"							FALSE	)
 
 OPTION(BUILD_OpenCart           "Build OpenCart Tools"                      FALSE   )
-
-OPTION(LOG_INFO					"Output Log info"							TRUE	)
-OPTION(LOG_INFO_THREAD			"Output Log info include ThreadPID"			TRUE	)
-OPTION(LOG_INFO_TIME			"Output Log info include time"				TRUE	)
-OPTION(LOG_INFO_SOURCE			"Output Log info include source and line"	OFF		)
-OPTION(LOG_FILE_ONLY_ERROR 		"Only log error to file"					TRUE	)
-OPTION(LOG_THREAD_MUTEX			"Log Thread Mutex"							TRUE	)
 OPTION(LOG_CDB_LOADER_LOG		"Output CDBLoader log"						FALSE	)
-
-IF(LOG_INFO)
-	MESSAGE("Output LogInfo")
-ELSE(LOG_INFO)
-	add_definitions("-DNO_LOGINFO")
-	MESSAGE("Don't output LogInfo")
-ENDIF(LOG_INFO)
-
-IF(LOG_INFO_THREAD)
-	add_definitions("-DLOG_INFO_THREAD")
-ENDIF(LOG_INFO_THREAD)
-
-IF(LOG_INFO_TIME)
-	add_definitions("-DLOG_INFO_TIME")
-ENDIF(LOG_INFO_TIME)
-
-IF(LOG_INFO_SOURCE)
-	add_definitions("-DLOG_INFO_SOURCE")
-ENDIF(LOG_INFO_SOURCE)
-
-IF(LOG_THREAD_MUTEX)
-	add_definitions("-DLOGINFO_THREAD_MUTEX")
-	MESSAGE("LogInfo use ThreadMutex")
-ELSE(LOG_THREAD_MUTEX)
-	MESSAGE("LogInfo don't use ThreadMutex")
-ENDIF(LOG_THREAD_MUTEX)
-
-if(LOG_FILE_ONLY_ERROR)
-	add_definitions("-DONLY_LOG_FILE_ERROR")
-	MESSAGE("File Log only record ERROR")
-else(LOG_FILE_ONLY_ERROR)
-	MESSAGE("File Log record all.")
-endif(LOG_FILE_ONLY_ERROR)
 
 if(LOG_CDB_LOADER_LOG)
 	add_definitions("-DLOG_CDB_LOADER_LOG")
---
+++ loginfo.cmake
@@ -0,0 +1,43 @@
+﻿SET(CMAKE_ALLOW_LOOSE_LOOP_CONSTRUCTS TRUE)
+
+OPTION(LOG_INFO					"Output Log info"							FALSE	)
+
+IF(LOG_INFO)
+	MESSAGE("Output LogInfo")
+
+    OPTION(LOG_INFO_THREAD			"Output Log info include ThreadPID"			TRUE	)
+    OPTION(LOG_INFO_TIME			"Output Log info include time"				TRUE	)
+    OPTION(LOG_INFO_SOURCE			"Output Log info include source and line"	OFF		)
+    OPTION(LOG_FILE_ONLY_ERROR 		"Only log error to file"					TRUE	)
+    OPTION(LOG_THREAD_MUTEX			"Log Thread Mutex"							TRUE	)
+
+    IF(LOG_INFO_THREAD)
+        add_definitions("-DLOG_INFO_THREAD")
+    ENDIF(LOG_INFO_THREAD)
+
+    IF(LOG_INFO_TIME)
+        add_definitions("-DLOG_INFO_TIME")
+    ENDIF(LOG_INFO_TIME)
+
+    IF(LOG_INFO_SOURCE)
+        add_definitions("-DLOG_INFO_SOURCE")
+    ENDIF(LOG_INFO_SOURCE)
+
+    IF(LOG_THREAD_MUTEX)
+        add_definitions("-DLOGINFO_THREAD_MUTEX")
+        MESSAGE("LogInfo use ThreadMutex")
+    ELSE(LOG_THREAD_MUTEX)
+        MESSAGE("LogInfo don't use ThreadMutex")
+    ENDIF(LOG_THREAD_MUTEX)
+
+    if(LOG_FILE_ONLY_ERROR)
+        add_definitions("-DONLY_LOG_FILE_ERROR")
+        MESSAGE("File Log only record ERROR")
+    else(LOG_FILE_ONLY_ERROR)
+        MESSAGE("File Log record all.")
+    endif(LOG_FILE_ONLY_ERROR)
+
+ELSE(LOG_INFO)
+	add_definitions("-DNO_LOGINFO")
+	MESSAGE("Don't output LogInfo")
+ENDIF(LOG_INFO)
