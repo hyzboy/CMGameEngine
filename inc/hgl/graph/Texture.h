8b7aa27a7a68c9db98940e5816a7f0d4729083f6
hyzboy <devnull@localhost>
2017-02-04 19:25:49 +0800

    删除在Texture类中的纹理过滤、创建参数、BITMAP等数据

--- inc/hgl/graph/Texture.h
+++ inc/hgl/graph/Texture.h
@@ -7,8 +7,6 @@ namespace hgl
 {
 	namespace graph
 	{
-		class Bitmap2D;
-
         extern int HGL_MAX_VS_TEXTURE_UNITS;        ///<Vertex Shader中的最大纹理单元
         extern int HGL_MAX_FS_TEXTURE_UNITS;        ///<Fragment Shader中的最大纹理单元
         extern int HGL_MAX_GS_TEXTURE_UNITS;        ///<Geometry Shader中的最大纹理单元
@@ -24,46 +22,32 @@ namespace hgl
 		*/
 		class Texture
 		{
-		public:
-
-			/**
-			* 贴图加载预处理
-			*/
-			enum LoadTextureProcess:uint
-			{
-				ltGenMipmaps=1<<0,		//生成mipmaps
-			};//enum LoadTextureProcess
-
 		protected:
+
+            uint type;
+            uint binding_type;
 
 			uint texture_id;																		///<纹理ID
 
 			uint color_format;																		///<色彩格式(如RED,RG,RGB,RGBA,SRGB,SRGBA之类)
 			uint video_format;																		///<显存格式
 
-			uint min_filter,mag_filter;																///<放大和缩小过滤方式
-
-			void *bitmap_data;																		///<原始点阵数据(不一定存在)
-
 		public:
 
 							Texture(uint t,uint bt);												//请使用CreateTexture()来创建贴图,函数在Render.h中定义
-			virtual			~Texture(){if(bitmap_data)hgl_free(bitmap_data);}
+			virtual			~Texture();
 
 		public:
 
 					int		GetID		    ()const{return texture_id;}								///<取得纹理ID
-			virtual	uint	GetType			()const=0;												///<取得类型
-			virtual	uint	GetBindingType	()const=0;												///<取得绑定类型
+			        uint	GetType			()const{return type;}									///<取得类型
+			        uint	GetBindingType	()const{return binding_type;}							///<取得绑定类型
 					uint	GetColorFormat	()const{return color_format;}							///<取得颜色格式
 					uint	GetVideoFormat	()const{return video_format;}							///<取得显存中的数据格式
 
-		public:
+		//public:
 
-			virtual void	SetMinFilter	(uint);													///<设置缩小过滤
-			virtual void	SetMagFilter	(uint);													///<设置放大过滤
-
-			virtual void	GetMipmapLevel	(int &,int &);											///<取得贴图mipmaps级别
+		//	virtual void	GetMipmapLevel	(int &,int &)=0;										///<取得贴图mipmaps级别
 		};//class Texture
 	}//namespace graph
 }//namespace hgl
