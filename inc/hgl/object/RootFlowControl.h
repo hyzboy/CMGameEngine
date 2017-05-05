6d82d8f7d9a9810049d2eba125d08bead077cee5
hyzboy <devnull@localhost>
2017-05-05 18:21:56 +0800

    增加RootFlowControl

---
+++ inc/hgl/object/RootFlowControl.h
@@ -0,0 +1,16 @@
+#ifndef HGL_ROOT_FLOW_CONTROL_INCLUDE
+#define HGL_ROOT_FLOW_CONTROL_INCLUDE
+
+namespace hgl
+{
+    /**
+    * 图形应用根流程控制器
+    */
+    class RootFlowControl:public FlowControl
+    {
+    public:
+
+
+    };//class RootFlowControl
+}//namespace hgl
+#endif//HGL_ROOT_FLOW_CONTROL_INCLUDE
