39775df7df48cf6c2882dadc5d539a73932379f9
hyzboy <devnull@localhost>
2017-05-31 10:32:30 +0800

    代码排版

--- src/Base/IO/FileOutputStream.cpp
+++ src/Base/IO/FileOutputStream.cpp
@@ -44,17 +44,17 @@ namespace hgl
             }
         }
 
-        int64    FileOutputStream::Write        (const void *buf,int64 size)            {return file?file->Write(buf,size):-1;}
+        int64   FileOutputStream::Write     (const void *buf,int64 size)			{return file?file->Write(buf,size):-1;}
 
-        bool    FileOutputStream::CanSeek    ()const                                    {return file?file->CanSeek():false;}
-        bool    FileOutputStream::CanRestart()const                                    {return file?file->CanRestart():false;}
-        bool    FileOutputStream::CanSize    ()const                                    {return file?file->CanSize():false;}
+        bool    FileOutputStream::CanSeek   ()const                                 {return file?file->CanSeek():false;}
+        bool    FileOutputStream::CanRestart()const                                 {return file?file->CanRestart():false;}
+        bool    FileOutputStream::CanSize   ()const                                 {return file?file->CanSize():false;}
 
-        int64    FileOutputStream::Tell        ()const                                    {return file?file->Tell():-1;}
-        int64    FileOutputStream::GetSize    ()const                                    {return file?file->GetSize():-1;}
-        bool    FileOutputStream::Restart    ()                                        {return file?file->Restart():false;}
-        int64    FileOutputStream::Seek        (int64 off,SeekOrigin so)                {return file?file->Seek(off,so):-1;}
+        int64   FileOutputStream::Tell      ()const                                 {return file?file->Tell():-1;}
+        int64   FileOutputStream::GetSize   ()const                                 {return file?file->GetSize():-1;}
+        bool    FileOutputStream::Restart   ()                                      {return file?file->Restart():false;}
+        int64   FileOutputStream::Seek      (int64 off,SeekOrigin so)               {return file?file->Seek(off,so):-1;}
 
-        int64    FileOutputStream::Write        (int64 off,const void *buf,int64 size)    {return file?file->Write(off,buf,size):-1;}
+        int64   FileOutputStream::Write		(int64 off,const void *buf,int64 size)  {return file?file->Write(off,buf,size):-1;}
     }//namespace io
 }//namespace hgl
