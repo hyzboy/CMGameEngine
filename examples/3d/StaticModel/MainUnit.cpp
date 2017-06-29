775ae9dcd4c6cf4fb610693f65d4a13f55f6fa40
hyzboy <devnull@localhost>
2017-06-29 17:19:15 +0800

    恢复CreateRenderableWireCube到可用

--- examples/3d/StaticModel/MainUnit.cpp
+++ examples/3d/StaticModel/MainUnit.cpp
@@ -1,6 +1,6 @@
 ﻿#include<hgl/Graphics.h>                //GraphicsApplication,SystemInitInfo
 #include<hgl/graph/Render.h>            //SetClearColor,ClearColorDepthBuffer
-#include<hgl/graph/Camera.h>            //WalkerCamera
+#include<hgl/graph/InlineRenderable.h>	//CreateRenderableWireCube
 #include<hgl/object/FlowObject.h>       //FlowObject
 
 #include"CameraControl.h"
@@ -15,21 +15,41 @@ const Vector3f  eye(100,100,80),
 
 class TestObject:public CameraControlFlowObject
 {
+	VertexArray *box_va;
+	Material *box_mtl;
+	Renderable *box_r;
+	Matrix4f box_mat;
 
 public:
 
 	TestObject()
 	{
-        SetClearColor(ceMozillaCharcoal);
+        SetClearColor(ceMozillaCharcoal);		//设置清屏颜色
+
+		box_va=CreateRenderableWireCube();
+
+		box_mtl=new Material;
+		box_mtl->SetColorMaterial(true);
+		box_mtl->SetColor(1,1,1,1);
+
+		box_r=new Renderable(box_va,box_mtl);
+		box_r->AutoCreateShader();
+
+		box_mat=scale(50,50,50);
 	}
 
 	~TestObject()
 	{
+		delete box_r;
+		delete box_va;
+		delete box_mtl;
 	}
 
 	void Draw(const Matrix4f *)
 	{
         ClearColorDepthBuffer();
+
+		DirectRender(box_r,&proj,&mv,&box_mat);            //渲染球列表
 	}
 };//class TestObject
 
