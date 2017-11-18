be470b0176d1495c3bf538b344904985136a86c3
hyzboy <hyzboy@gmail.com>
2017-11-18 15:04:04 +0800

    变量名称修正

--- data/shader/deferred.gbuffer.depth_to_pos.glsl
+++ data/shader/deferred.gbuffer.depth_to_pos.glsl
@@ -1,8 +1,8 @@
-vec3 DepthToPos(float depth, vec2 texCoord)
+vec3 DepthToPos(in float depth, in vec2 texCoord)
 {
     vec4 ndcspace = vec4(texCoord.x * 2.0 - 1.0, texCoord.y * 2.0 - 1.0, depth * 2.0 - 1.0, 1.0);
 
-    vec4 temp = inverse(MatView) * inverse(MatProj) * ndcspace;
+    vec4 temp = inverse(ModelViewMatrix) * inverse(ProjectionMatrix) * ndcspace;
 
     return temp.xyz / temp.w;
 }
