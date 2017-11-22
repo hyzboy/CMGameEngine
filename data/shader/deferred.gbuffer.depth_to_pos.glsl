c965a7bb628039c9058ec61a19b79bbaa9dd5727
hyzboy <hyzboy@gmail.com>
2017-11-22 11:02:56 +0800

    改进函数命名

--- data/shader/deferred.gbuffer.depth_to_pos.glsl
+++ data/shader/deferred.gbuffer.depth_to_pos.glsl
@@ -1,6 +1,6 @@
-vec3 DepthToPos(in float depth, in vec2 texCoord)
+vec3 depth_to_pos(in float depth, in vec2 ss_pos)
 {
-    vec4 ndcspace = vec4(texCoord.x * 2.0 - 1.0, texCoord.y * 2.0 - 1.0, depth * 2.0 - 1.0, 1.0);
+    vec4 ndcspace = vec4(ss_pos.x * 2.0 - 1.0, ss_pos.y * 2.0 - 1.0, depth * 2.0 - 1.0, 1.0);
 
     vec4 temp = inverse(ModelViewMatrix) * inverse(ProjectionMatrix) * ndcspace;
 
