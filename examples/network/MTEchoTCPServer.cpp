94a9a4b1dca4d9e3ed41964b48504179939e3102
hyzboy <devnull@localhost>
2017-12-01 18:08:27 +0800

    Network部分增加MTEchoServer/TCPClientHello范例

---
+++ examples/network/MTEchoTCPServer.cpp
@@ -0,0 +1,67 @@
+﻿#include<hgl/Console.h>
+#include<hgl/Time.h>
+#include<hgl/network/MTTCPServer.h>
+#include<iostream>
+
+using namespace hgl;
+using namespace hgl::network;
+
+class UserAccept:public TCPAccept
+{
+public:
+
+    using TCPAccept::TCPAccept;
+    ~UserAccept()=default;
+
+    bool OnRecvPacket(void *data,const PACKET_SIZE_TYPE &size) override
+    {
+        std::string str((char *)data,size);
+
+        std::cout<<"recv packet "<<str.c_str()<<std::endl;
+
+        char *s=(char *)data;
+        char *e=s+size-1;
+        char t;
+
+        while(s<e)
+        {
+            t=*s;
+            *s=*e;
+            *e=t;
+
+            ++s;
+            --e;
+        }
+
+        return SendPacket(data,size);       //再发回去
+    }
+};//class UserAccept:public TCPAccept
+
+HGL_CONSOLE_MAIN(sii,app,args)
+{
+    sii.info.ProjectName=U8_TEXT("多线程Echo TCP服务器");
+    sii.info.ProjectCode=OS_TEXT("MTEchoTCPServer");
+
+    if(!app.Init(&sii))
+        return(-1);
+
+    IPAddress *server_ip;
+
+    server_ip=CreateIPv4TCP(10240);
+
+    if(!server_ip)
+        return(-2);
+
+    MTTCPServer<UserAccept> server;
+    MTTCPServer<UserAccept>::InitInfomation info;
+
+    info.server_ip=server_ip;
+    info.thread_count=4;
+
+    if(!server.Init(info))
+        return(-3);
+
+    while(server.Wait());      //等待服务器关闭
+    return(0);
+}
+
---
+++ examples/network/TCPClientHello.cpp
@@ -0,0 +1,57 @@
+﻿#include<hgl/Console.h>
+#include<hgl/network/TCPClient.h>
+#include<hgl/io/DataOutputStream.h>
+#include<hgl/io/DataInputStream.h>
+
+#include<iostream>
+
+using namespace hgl;
+using namespace hgl::network;
+
+using namespace std;
+
+HGL_CONSOLE_MAIN(sii,app,args)
+{
+    sii.info.ProjectName=U8_TEXT("接入测试客户端");
+    sii.info.ProjectCode=OS_TEXT("TCPClientHello");
+
+    if(!app.Init(&sii))
+        return(-1);
+
+    IPAddress *ip=CreateIPv4TCP("127.0.0.1",10240);
+
+    TCPClient client;
+
+    if(!client.Connect(ip))
+    {
+        cout<<"connect error"<<endl;
+        return(-2);
+    }
+
+    cout<<"connect ok"<<endl;
+
+    //发送
+    {
+        const UTF8String str="Hello,World!";
+
+        io::LEDataOutputStream dos(client.GetOutputStream());
+        if(dos.WriteUTF8String(str))
+            cout<<"send ok,str: "<<str.c_str()<<endl;
+    }
+
+    int result=client.WaitRecv(60);
+
+    if(result>0)//接收
+    {
+        UTF8String str;
+
+        io::LEDataInputStream dis(client.GetInputStream());
+
+        if(dis.ReadUTF8String(str))
+            cout<<"recv ok,str: "<<str.c_str()<<endl;
+    }
+
+    client.Disconnect();
+    return 0;
+}
+
