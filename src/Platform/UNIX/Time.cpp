67f5d97d1c437a65edd17063362046efcb58019b
hyzboy <hyzboy@gmail.com>
2017-12-05 11:36:47 +0800

    SetTimeVal改为多系统通用

--- src/Platform/UNIX/Time.cpp
+++ src/Platform/UNIX/Time.cpp
@@ -17,12 +17,6 @@ namespace hgl
 			++abstime.tv_sec;
 		}
 	}
-
-	void SetTimeVal(timeval &tv,const double t_sec)
-	{
-		tv.tv_sec=t_sec;
-		tv.tv_usec=(t_sec-(double)tv.tv_sec)*HGL_MICRO_SEC_PER_SEC;
-	}
 }//namespace hgl
 
 namespace hgl
