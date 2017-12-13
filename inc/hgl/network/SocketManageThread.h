1188c177756597011f1c182ba416ce554d71bbcf
hyzboy <hyzboy@gmail.com>
2017-12-13 14:35:00 +0800

    因转为模板，将SocketManageThread.cpp代码移置.h

--- inc/hgl/network/SocketManageThread.h
+++ inc/hgl/network/SocketManageThread.h
@@ -48,9 +48,44 @@ namespace hgl
 
             virtual bool Join(USER_ACCEPT *us){return sock_manage->Join(us);}     ///<单个工作对象接入处理函数
             virtual bool Unjoin(USER_ACCEPT *us){return sock_manage->Unjoin(us);} ///<单个工作对象退出处理函数
+                        
+            /**
+             *处理要接入的工作对象列表
+             */
+            void ProcJoinList()
+            {
+                AcceptSocketList &usl=join_list.GetReceive();
 
-            void ProcJoinList();                                                ///<处理要接入的工作对象列表
-            void ProcUnjoinList();                                              ///<处理要退出的工作对象列表
+                const int count=usl.GetCount();
+                TCPAccept **us=usl.GetData();
+
+                for(int i=0;i<count;i++)
+                {
+                    Join(*us);
+                    ++us;
+                }
+
+                usl.ClearData();
+            }
+
+            /**
+             * 处理要退出的工作对象列表
+             */
+            void ProcUnjoinList()
+            {
+                AcceptSocketList &usl=unjoin_list.GetReceive();
+
+                const int count=usl.GetCount();
+                TCPAccept **us=usl.GetData();
+
+                for(int i=0;i<count;i++)
+                {
+                    Unjoin(*us);
+                    ++us;
+                }
+
+                usl.ClearData();
+            }
 
         public:
 
@@ -64,8 +99,31 @@ namespace hgl
                 SAFE_CLEAR(sock_manage);
             }
 
-            virtual void ProcEndThread() override;
-            virtual bool Execute() override;
+            virtual void ProcEndThread() override            
+            {
+                ClearAcceptSocketList(join_list.GetReceive());
+                join_list.Swap();
+                ClearAcceptSocketList(join_list.GetReceive());
+
+                sock_manage->Clear();
+
+                //unjoin_list中的理论上都已经在wo_list/join_list里了，所以不需要走Clear，直接清空列表
+                unjoin_list.GetReceive().ClearData();
+                unjoin_list.Swap();
+                unjoin_list.GetReceive().ClearData();
+            }
+
+            virtual bool Execute() override
+            {
+                if(join_list.TrySemSwap())
+                    ProcJoinList();
+
+                if(unjoin_list.TrySemSwap())
+                    ProcUnjoinList();
+
+                sock_manage->Update(0.1);         //这里写0.1秒，只是为了不卡住主轮循。未来要将epoll(recv)完全独立一个线程跑
+                return(true);
+            }
 
         public:
 
