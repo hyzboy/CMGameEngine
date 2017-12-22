235b35aa6c8637ef41ed1753ade31e60957d87fc
hyzboy <hyzboy@gmail.com>
2017-12-22 14:46:00 +0800

    改进代码使其在Win下也可编译、运行、正常输出到控制台

--- tools/TextEncodingConvert/MainUnit.cpp
+++ tools/TextEncodingConvert/MainUnit.cpp
@@ -4,6 +4,13 @@
 #include<hgl/io/FileOutputStream.h>
 #include<hgl/io/TextOutputStream.h>
 
+#if HGL_OS == HGL_OS_Windows
+    #include<wchar.h>
+    #define cmd_out std::wcout
+#else
+    #define cmd_out std::cout
+#endif//
+
 using namespace hgl;
 using namespace hgl::io;
 using namespace hgl::filesystem;
@@ -28,14 +35,14 @@ static bool include_bom=true;
 static CharSet in_charset;
 static CharSet out_charset;
 
-void ConvertQtoB(char16_t *str,int len)
+void ConvertQtoB(u16char *str,int len)
 {
-    constexpr char16_t QC[]=u"ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ１２３４５６７８９０";
-    constexpr char16_t BC[]=u"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+    constexpr u16char QC[]=U16_TEXT("ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ１２３４５６７８９０");
+    constexpr u16char BC[]=U16_TEXT("ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890");
 
-    const int tl=sizeof(QC)/sizeof(char16_t)-1;
+    const int tl=sizeof(QC)/sizeof(u16char)-1;
 
-    char16_t *p=str;
+    u16char *p=str;
 
     while(len--)
     {
@@ -83,12 +90,12 @@ EnumFileConfig *ConvertSubFolderConfig(struct EnumFileConfig *efc,const OSString
 
     if(!MakePath(new_path))
     {
-        std::cout<<"[ERR] Create Directory failed: "<<new_path.c_str()<<std::endl;
+        cmd_out<<OS_TEXT("[ERR] Create Directory failed: ")<<new_path.c_str()<<std::endl;
         return(nullptr);
     }
     else
     {
-        std::cout<<"[DIR] Create Directory: "<<new_path.c_str()<<std::endl;
+        cmd_out<<OS_TEXT("[DIR] Create Directory: ")<<new_path.c_str()<<std::endl;
     }
 
     return DefaultCreateSubConfig(efc,sub_folder_name);
@@ -122,7 +129,7 @@ template<typename TOS> void Convert(const OSString &in_filename,const OSString &
 
     if(!fos.CreateTrunc(out_filename))
     {
-        std::cout<<"[ERR] Create file failed: "<<out_filename.c_str()<<std::endl;
+        cmd_out<<OS_TEXT("[ERR] Create file failed: ")<<out_filename.c_str()<<std::endl;
         return;
     }
 
@@ -133,11 +140,11 @@ template<typename TOS> void Convert(const OSString &in_filename,const OSString &
 
     if(!tos.WriteText(old_text))
     {
-        std::cout<<"[ERR] Write Text failed"<<std::endl;
+        cmd_out<<OS_TEXT("[ERR] Write Text failed")<<std::endl;
         return;
     }
 
-    std::cout<<"[OK] filename: "<<out_filename.c_str()<<std::endl;
+    cmd_out<<OS_TEXT("[OK] filename: ")<<out_filename.c_str()<<std::endl;
 };
 
 void ConvertFile(struct EnumFileConfig *efc,FileInfo &fi)
@@ -150,13 +157,13 @@ void ConvertFile(struct EnumFileConfig *efc,FileInfo &fi)
             return;
     }
 
-    const char *ext_name=strrchr<os_char>(fi.name,'.');
+    const os_char *ext_name=strrchr<os_char>(fi.name,'.');
 
     if(!ext_name)return;
 
     if(!ExtNameCheck(ext_name))return;
 
-    std::cout<<"[IN] filename: "<<fi.fullname<<std::endl;
+    cmd_out<<OS_TEXT("[IN] filename: ")<<fi.fullname<<std::endl;
 
     //生成新文件名
 
@@ -182,12 +189,12 @@ void ConvertFile(struct EnumFileConfig *efc,FileInfo &fi)
 
 void out_string_list(const OSStringList &sl,const os_char *name)
 {
-    std::cout<<" "<<name<<": "<<std::endl;
+    cmd_out<<OS_TEXT(" ")<<name<<OS_TEXT(": ")<<std::endl;
 
     for(int i=0;i<sl.GetCount();i++)
     {
         if(sl[i].Length()<=0)continue;
-        std::cout<<"\t"<<i<<": "<<sl[i].c_str()<<std::endl;
+        cmd_out<<OS_TEXT("\t")<<i<<OS_TEXT(": ")<<sl[i].c_str()<<std::endl;
     }
 
     std::cout<<std::endl;
@@ -226,53 +233,57 @@ HGL_CONSOLE_MAIN_FUNC()
         return(0);
     }
 
-    if(args.CaseFind("-fjh")!=-1)
+    if(args.CaseFind(OS_TEXT("-fjh"))!=-1)
     {
         fn_japanese_hw2fw=true;
 
         std::cout<<"option[-fjh]: conevrt shift-jis half-width japanese filename to full-width"<<std::endl;
     }
 
-    if(args.CaseFind("-jh")!=-1)
+    if(args.CaseFind(OS_TEXT("-jh"))!=-1)
     {
         japanese_hw2fw=true;
 
         std::cout<<"option[-jh]: conevrt shift-jis half-width japanese text to full-width"<<std::endl;
     }
 
-    if(args.CaseFind("-fbl")!=-1)
+    if(args.CaseFind(OS_TEXT("-fbl"))!=-1)
     {
         fn_english_fw2hw=true;
 
         std::cout<<"option[-fbl]: conevrt full-width english and number filename to half-width"<<std::endl;
     }
 
-    if(args.CaseFind("-bl")!=-1)
+    if(args.CaseFind(OS_TEXT("-bl"))!=-1)
     {
         english_fw2hw=true;
 
         std::cout<<"option[-bl]: conevrt full-width english and number text to half-width"<<std::endl;
     }
 
-    if(args.CaseFind("-no_bom")!=-1)
+    if(args.CaseFind(OS_TEXT("-no_bom"))!=-1)
     {
         include_bom=false;
 
         std::cout<<"option[-no_bom]: don't include BOM Header"<<std::endl;
     }
 
-    if(args.CaseFind("-sf")!=-1)
+    if(args.CaseFind(OS_TEXT("-sf"))!=-1)
     {
         sub_folder=true;
 
         std::cout<<"option[-sf]: proc sub-folder"<<std::endl;
     }
 
-    int in_off=args.CaseFind("-in");
+    int in_off=args.CaseFind(OS_TEXT("-in"));
 
     if(in_off!=-1)
     {
+#if HGL_OS == HGL_OS_Windows        
+        const UTF8String in_cs_str=to_u8(args[in_off+1]);
+#else
         const OSString &in_cs_str=args[in_off+1];
+#endif//
 
         in_charset=CharSet(in_cs_str);
 
@@ -285,11 +296,15 @@ HGL_CONSOLE_MAIN_FUNC()
         std::cout<<"in CharSet: "<<in_charset.charset<<std::endl;
     }
 
-    int out_off=args.CaseFind("-out");
+    int out_off=args.CaseFind(OS_TEXT("-out"));
 
     if(out_off!=-1)
     {
+#if HGL_OS == HGL_OS_Windows        
+        const UTF8String out_cs_str=to_u8(args[out_off+1]);
+#else
         const OSString &out_cs_str=args[out_off+1];
+#endif//
 
         if(out_cs_str.CaseComp("utf8")==0)out_charset=CharSet(ccpUTF8);else
         if(out_cs_str.CaseComp("utf16")==0
@@ -308,8 +323,8 @@ HGL_CONSOLE_MAIN_FUNC()
     in_folder_base=args[in_off+2];
     out_folder_base=args[out_off+2];
 
-    std::cout<<"in folder: "<<in_folder_base.c_str()<<std::endl;
-    std::cout<<"out folder: "<<out_folder_base.c_str()<<std::endl;
+    cmd_out<<OS_TEXT("in folder: ")<<in_folder_base.c_str()<<std::endl;
+    cmd_out<<OS_TEXT("out folder: ")<<out_folder_base.c_str()<<std::endl;
 
     if(!MakePath(out_folder_base))
     {
