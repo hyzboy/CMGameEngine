68909be080a421c34cde6863fe9e2dfc1883a172
hyzboy <devnull@localhost>
2017-12-22 10:58:41 +0800

    TextEncodingConvert初步可用版本，日文转换尚未实现

--- tools/TextEncodingConvert/MainUnit.cpp
+++ tools/TextEncodingConvert/MainUnit.cpp
@@ -1,8 +1,11 @@
 ﻿#include<hgl/Console.h>
 #include<hgl/type/StringList.h>
 #include<hgl/FileSystem.h>
+#include<hgl/io/FileOutputStream.h>
+#include<hgl/io/TextOutputStream.h>
 
 using namespace hgl;
+using namespace hgl::io;
 using namespace hgl::filesystem;
 
 static OSStringList convert_ext_list;       //要转换的文件
@@ -10,13 +13,44 @@ static OSStringList convert_ext_list;       //要转换的文件
 static OSStringList discard_file_list;      //要抛弃的文件
 static OSStringList discard_folder_list;    //要抛弃的目录
 
+static OSString in_folder_base;
+static OSString out_folder_base;
+
+static bool fn_japanese_hw2fw=false;
+static bool fn_english_fw2hw=false;
+
 static bool japanese_hw2fw=false;
 static bool english_fw2hw=false;
+
 static bool sub_folder=false;
 static bool include_bom=true;
 
 static CharSet in_charset;
 static CharSet out_charset;
+
+void ConvertQtoB(char16_t *str,int len)
+{
+    constexpr char16_t QC[]=u"ＡＢＣＤＥＦＧＨＩＪＫＬＭＮＯＰＱＲＳＴＵＶＷＸＹＺａｂｃｄｅｆｇｈｉｊｋｌｍｎｏｐｑｒｓｔｕｖｗｘｙｚ１２３４５６７８９０";
+    constexpr char16_t BC[]=u"ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz1234567890";
+
+    const int tl=sizeof(QC)/sizeof(char16_t)-1;
+
+    char16_t *p=str;
+
+    while(len--)
+    {
+        for(int i=0;i<tl;i++)
+        {
+            if(*p==QC[i])
+            {
+                *p=BC[i];
+                break;
+            }
+        }
+
+        p++;
+    }
+}
 
 EnumFileConfig *ConvertSubFolderConfig(struct EnumFileConfig *efc,const OSString &sub_folder_name)
 {
@@ -26,6 +60,35 @@ EnumFileConfig *ConvertSubFolderConfig(struct EnumFileConfig *efc,const OSString
     {
         if(discard_folder_list[i].Comp(sub_folder_name)==0)
             return(nullptr);
+    }
+
+    OSString old_path;
+    OSString new_path;
+
+    MergeFilename(old_path,efc->folder_name,sub_folder_name);
+    MergeFilename(new_path,out_folder_base,old_path.c_str()+in_folder_base.Length());
+
+#if HGL_OS != HGL_OS_Windows
+    UTF16String u16_path=to_u16(new_path);
+
+    if(fn_english_fw2hw)
+    {
+        ConvertQtoB(u16_path.c_str(),u16_path.Length());
+
+        new_path=to_u8(u16_path);
+    }
+#else
+    ConvertQtoB(new_path.c_str(),new_path.Length());
+#endif//
+
+    if(!MakePath(new_path))
+    {
+        std::cout<<"[ERR] Create Directory failed: "<<new_path.c_str()<<std::endl;
+        return(nullptr);
+    }
+    else
+    {
+        std::cout<<"[DIR] Create Directory: "<<new_path.c_str()<<std::endl;
     }
 
     return DefaultCreateSubConfig(efc,sub_folder_name);
@@ -48,6 +111,35 @@ bool ExtNameCheck(const os_char *ext_name)
     return(false);
 }
 
+template<typename TOS> void Convert(const OSString &in_filename,const OSString &out_filename)
+{
+    UTF16StringList old_text;
+
+    if(LoadStringListFromTextFile(old_text,in_filename,in_charset)<=0)
+        return;
+
+    FileOutputStream fos;
+
+    if(!fos.CreateTrunc(out_filename))
+    {
+        std::cout<<"[ERR] Create file failed: "<<out_filename.c_str()<<std::endl;
+        return;
+    }
+
+    TOS tos(&fos);
+
+    if(include_bom)
+        tos.WriteBOM();
+
+    if(!tos.WriteText(old_text))
+    {
+        std::cout<<"[ERR] Write Text failed"<<std::endl;
+        return;
+    }
+
+    std::cout<<"[OK] filename: "<<out_filename.c_str()<<std::endl;
+};
+
 void ConvertFile(struct EnumFileConfig *efc,FileInfo &fi)
 {
     if(fi.name[0]=='.')return;
@@ -64,7 +156,28 @@ void ConvertFile(struct EnumFileConfig *efc,FileInfo &fi)
 
     if(!ExtNameCheck(ext_name))return;
 
-    std::cout<<"filename: "<<fi.fullname<<std::endl;
+    std::cout<<"[IN] filename: "<<fi.fullname<<std::endl;
+
+    //生成新文件名
+
+    OSString new_filename=out_folder_base+(fi.fullname+in_folder_base.Length());
+
+    #if HGL_OS != HGL_OS_Windows
+    UTF16String u16_filename=to_u16(new_filename);
+
+    if(fn_english_fw2hw)
+    {
+        ConvertQtoB(u16_filename.c_str(),u16_filename.Length());
+
+        new_filename=to_u8(u16_filename);
+    }
+    #else
+    ConvertQtoB(new_filename.c_str(),new_filename.Length());
+    #endif//
+
+    if(out_charset.codepage==ccpUTF8    )Convert<UTF8TextOutputStream   >(fi.fullname,new_filename);else
+    if(out_charset.codepage==ccpUTF16LE )Convert<UTF16LETextOutputStream>(fi.fullname,new_filename);else
+    if(out_charset.codepage==ccpUTF16BE )Convert<UTF16BETextOutputStream>(fi.fullname,new_filename);
 }
 
 void out_string_list(const OSStringList &sl,const os_char *name)
@@ -94,12 +207,14 @@ HGL_CONSOLE_MAIN_FUNC()
     if(args.GetCount()<5)
     {
         std::cout<<" option:"<<std::endl;
-        std::cout<<"\t-jh\tconvert shift-jis half-width japanese characters to full-width"<<std::endl;        //转换日文半角到全角
-        std::cout<<"\t-bl\tconvert full-width english and number to half-width"<<std::endl;                   //转换全角英文和数字到半角
-        std::cout<<"\t-no_bom\ttarget file add BOM header"<<std::endl;                                        //新文件不需要BOM头
-        std::cout<<"\t-sf\tproc sub-folder"<<std::endl;                                                       //处理子目录
+        std::cout<<"\t-fjh\tconvert shift-jis half-width japanese filename to full-width"<<std::endl;       //转换日文半角文件名到全角
+        std::cout<<"\t-jh\tconvert shift-jis half-width japanese text to full-width"<<std::endl;            //转换日文半角文本到全角
+        std::cout<<"\t-fbl\tconvert full-width english and number filename to half-width"<<std::endl;       //转换全角英文和数字文件名到半角
+        std::cout<<"\t-bl\tconvert full-width english and number text to half-width"<<std::endl;            //转换全角英文和数字文本到半角
+        std::cout<<"\t-no_bom\ttarget file add BOM header"<<std::endl;                                      //新文件不需要BOM头
+        std::cout<<"\t-sf\tproc sub-folder"<<std::endl;                                                     //处理子目录
         std::cout<<std::endl;
-        std::cout<<" Format:  TextEncodingConvert [-jh|-bl|-sf] -in <shift_jis|big5|gbk|...> <input path> -out <utf8|utf16le> <output path>"<<std::endl;
+        std::cout<<" Format:  TextEncodingConvert [-fjh|-jh|-fbl|-bl|-no_bom|-sf] -in <shift_jis|big5|gbk|...> <input path> -out <utf8|utf16le> <output path>"<<std::endl;
         std::cout<<" Example: TextEncodingConvert -in shift_jis -no_bom /home/hyzboy/input/ -out utf8 /home/hyzboy/output/"<<std::endl;
         std::cout<<std::endl;
 
@@ -111,18 +226,32 @@ HGL_CONSOLE_MAIN_FUNC()
         return(0);
     }
 
+    if(args.CaseFind("-fjh")!=-1)
+    {
+        fn_japanese_hw2fw=true;
+
+        std::cout<<"option[-fjh]: conevrt shift-jis half-width japanese filename to full-width"<<std::endl;
+    }
+
     if(args.CaseFind("-jh")!=-1)
     {
         japanese_hw2fw=true;
 
-        std::cout<<"option[-jh]: conevrt shift-jis half-width japanese characters to full-width"<<std::endl;
+        std::cout<<"option[-jh]: conevrt shift-jis half-width japanese text to full-width"<<std::endl;
+    }
+
+    if(args.CaseFind("-fbl")!=-1)
+    {
+        fn_english_fw2hw=true;
+
+        std::cout<<"option[-fbl]: conevrt full-width english and number filename to half-width"<<std::endl;
     }
 
     if(args.CaseFind("-bl")!=-1)
     {
         english_fw2hw=true;
 
-        std::cout<<"option[-bl]: conevrt full-width english and number to half-width"<<std::endl;
+        std::cout<<"option[-bl]: conevrt full-width english and number text to half-width"<<std::endl;
     }
 
     if(args.CaseFind("-no_bom")!=-1)
@@ -176,16 +305,23 @@ HGL_CONSOLE_MAIN_FUNC()
         std::cout<<"out CharSet: "<<out_charset.charset<<std::endl;
     }
 
-    const OSString in_folder=args[in_off+2];
+    in_folder_base=args[in_off+2];
+    out_folder_base=args[out_off+2];
 
-    std::cout<<"in folder: "<<in_folder.c_str()<<std::endl;
-    std::cout<<"out folder: "<<args[out_off+2].c_str()<<std::endl;
+    std::cout<<"in folder: "<<in_folder_base.c_str()<<std::endl;
+    std::cout<<"out folder: "<<out_folder_base.c_str()<<std::endl;
+
+    if(!MakePath(out_folder_base))
+    {
+        std::cout<<"[ERR] Create Directory failed: "<<out_folder_base.c_str()<<std::endl;
+        return(-3);
+    }
 
     std::cout<<std::endl;
 
     EnumFileConfig efc;
 
-    efc.folder_name=in_folder;
+    efc.folder_name=in_folder_base;
     efc.sub_folder=sub_folder;
 
     efc.CreateSubConfig=ConvertSubFolderConfig;
