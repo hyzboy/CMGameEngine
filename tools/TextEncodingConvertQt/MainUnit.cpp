ef48288e4ae1861a1103e2afe94818c898b43842
hyzboy <hyzboy@gmail.com>
2018-01-09 14:33:57 +0800

    增加TexEncodingConvertQt空工程文件

---
+++ tools/TextEncodingConvertQt/MainUnit.cpp
@@ -0,0 +1,15 @@
+﻿#include"MainWindow.h"
+#include<hgl/QT.h>
+
+HGL_QT_MAIN_FUNC()
+{
+	sii.info.ProjectName=U8_TEXT("文本编码转换工具");
+	sii.info.ProjectCode=OS_TEXT("TextEncodingConvert");
+
+	app.Init(&sii);
+
+    MainWindow foo;
+
+    foo.Init();
+    return app.exec();
+}
---
+++ tools/TextEncodingConvertQt/MainWindow.cpp
@@ -0,0 +1,15 @@
+#include"MainWindow.h"
+
+MainWindow::MainWindow()
+{
+}
+
+MainWindow::~MainWindow()
+{
+}
+
+void MainWindow::Init()
+{
+    this->setWindowTitle("TextEncodingConvert");
+    this->resize(400,300);
+}
