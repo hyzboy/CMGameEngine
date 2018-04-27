aa1f41d4b992d88be166bd23311310420be6671b
HuYingzhuo <hyzboy@gmail.com>
2018-04-27 21:58:07 +0800

    文件编码改为utf8，并去除注释中的中文符号

--- src/Algorithm/LAtan.cpp
+++ src/Algorithm/LAtan.cpp
@@ -1,4 +1,4 @@
-#include<hgl/algorithm/VectorMath.h>
+ï»¿#include<hgl/algorithm/VectorMath.h>
 #include<cmath>
 namespace hgl
 {
@@ -6,27 +6,27 @@ namespace hgl
     {
         /**
          * fast atan2
-         * from£ºJim Shima
-         * date£º1999/04/23
+         * from: Jim Shima
+         * date: 1999/04/23
          */
-        double Latan2(double y, double x) 
+        double Latan2(double y, double x)
         {
             double abs_y = fabs(y) + 1e-10;      // kludge to prevent 0/0 condition
             double angle;
             double r;
 
-            if (x >= 0) 
+            if (x >= 0)
             {
                 r = (x - abs_y) / (x + abs_y);
                 angle = HGL_PI_4 - HGL_PI_4 * r;
-            } 
-            else 
+            }
+            else
             {
                 r = (x + abs_y) / (abs_y - x);
                 angle = HGL_PI_3_4 - HGL_PI_4 * r;
             }
 
-            if (y < 0) 
+            if (y < 0)
                 return (-angle);     // negate if in quad III or IV
             else
                 return (angle);
