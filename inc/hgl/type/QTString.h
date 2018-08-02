f186abd60f99959d8da165f6df64cd969a332cec
hyzboy <hyzboy@gmail.com>
2018-08-02 13:00:50 +0800

    修正QTString.h名字空间问题

--- inc/hgl/type/QTString.h
+++ inc/hgl/type/QTString.h
@@ -1,7 +1,8 @@
-#ifndef HGL_QT_STRING_INCLUDE
+﻿#ifndef HGL_QT_STRING_INCLUDE
 #define HGL_QT_STRING_INCLUDE
 
 #include<hgl/type/BaseString.h>
+#include<hgl/CodePage.h>
 #include<QString>
 
 #if HGL_OS == HGL_OS_Windows
@@ -11,21 +12,21 @@
 	}
 
 	inline QString toQString(const wchar_t *str,int size){return QString::fromWCharArray(str,size);}
-	
-    inline UTF8String ToUTF8String(const QString &str)
+
+    inline hgl::UTF8String ToUTF8String(const QString &str)
     {
         QByteArray u8str=str.toUtf8();
-        
-        return UTF8String(u8str.data(),u8str.size());
+
+        return hgl::UTF8String(u8str.data(),u8str.size());
     }
 
-    inline WideString ToOSString(const QString &str)
+    inline hgl::WideString ToOSString(const QString &str)
     {
         //qt有toWCharArray(),但是不能预估wchar_t的长度，所以暂时使用先UTF8，再转utf16的方式
-        
+
         QByteArray u8str=str.toUtf8();
-        
-        return to_u16(u8str.data(),u8str.size());
+
+        return hgl::to_u16(u8str.data(),u8str.size());
     }
 #else
 	template<int WS> QString WCharToQString(const wchar_t *,int);
@@ -45,19 +46,18 @@
 	{
 		return QString::fromUtf16((ushort *)str.c_str(), str.Length());
 	}
-	
-    inline UTF8String ToUTF8string(const QString &str)
+
+    inline hgl::UTF8String ToUTF8String(const QString &str)
     {
         QByteArray u8str=str.toUtf8();
-        
-        return UTF8String(u8str.data(),u8str.size());        
+
+        return hgl::UTF8String(u8str.data(),u8str.size());
     }
 
-    inline UTF8String ToOSString(const QString &str)
+    inline hgl::UTF8String ToOSString(const QString &str)
     {
         return ToUTF8String(str);
-    }   
-
+    }
 #endif//HGL_OS == HGL_OS_Windows
 
 inline QString toQString(const hgl::UTF8String &str)
