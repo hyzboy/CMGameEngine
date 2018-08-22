1df7a9eb44ab12a2e574d4744a2dfc8539ba262b
hyzboy <hyzboy@gmail.com>
2018-08-22 13:10:19 +0800

    使范例strchr_test输出结果更加规范

--- examples/datatype/strchr_test.cpp
+++ examples/datatype/strchr_test.cpp
@@ -2,32 +2,42 @@
 #include<hgl/FileSystem.h>
 #include<iostream>
 
+void out(const char *source,const char *result)
+{
+    std::cout<<source<<" return \""<<result<<"\""<<std::endl;
+}
+
+void out(const char *source,const hgl::UTF8String &param,const hgl::UTF8String &result)
+{
+    std::cout<<source<<"(\""<<param.c_str()<<"\") return \""<<result.c_str()<<"\""<<std::endl;
+}
+
 int main(int,char **)
 {
     constexpr char str[]="hello,world!";
 
-    std::cout<<"str: "<<str<<std::endl;
+    std::cout<<"str: \""<<str<<"\""<<std::endl;
 
-    std::cout<<"strchr(str,',') return "<<hgl::strchr(str,',')<<std::endl;
-    std::cout<<"strrchr(str,strlen(str),',') return "<<hgl::strrchr(str,strlen(str),',')<<std::endl;
+    out("strchr(str,',')",hgl::strchr(str,','));
+    out("strrchr(str,strlen(str),',')",hgl::strrchr(str,strlen(str),','));
 
-    std::cout<<"strchr(str,\"el\",2) return "<<hgl::strchr(str,"el",2)<<std::endl;
-    std::cout<<"strrchr(str,strlen(str),\"el\",2) return "<<hgl::strrchr(str,strlen(str),"el",2)<<std::endl;
+    out("strchr(str,\"el\",2)",hgl::strchr(str,"el",2));
+    out("strrchr(str,strlen(str),\"el\",2)",hgl::strrchr(str,strlen(str),"el",2));
 
-    std::cout<<"strrchr(str,strlen(str),3,'l') return "<<hgl::strrchr(str,strlen(str),3,'l')<<std::endl;
-    std::cout<<"strrchr(str,strlen(str),3,\"el\",2) return "<<hgl::strrchr(str,strlen(str),3,"el",2)<<std::endl;
+    out("strrchr(str,strlen(str),3,'l')",hgl::strrchr(str,strlen(str),3,'l'));
+    out("strrchr(str,strlen(str),3,\"el\",2)",hgl::strrchr(str,strlen(str),3,"el",2));
 
     hgl::UTF8String fn("C:\\1.txt");
 
-    std::cout<<"ClipFilename(\""<<fn.c_str()<<"\") return "<<hgl::filesystem::ClipFilename(fn)<<std::endl;
+    out("ClipFilename",fn,hgl::filesystem::ClipFilename(fn));
 
     hgl::UTF8String tp1("C:\\1\\2");
     hgl::UTF8String tp2("C:\\1\\2\\");
     hgl::UTF8String tp3("C:\\1\\2\\\\");
 
-    std::cout<<"ClipLastPathname(\""<<tp1.c_str()<<"\") return "<<hgl::filesystem::ClipLastPathname(tp1).c_str()<<std::endl;
-    std::cout<<"ClipLastPathname(\""<<tp2.c_str()<<"\") return "<<hgl::filesystem::ClipLastPathname(tp2).c_str()<<std::endl;
-    std::cout<<"ClipLastPathname(\""<<tp3.c_str()<<"\") return "<<hgl::filesystem::ClipLastPathname(tp3).c_str()<<std::endl;
+    out("ClipLastPathname",tp1,hgl::filesystem::ClipLastPathname(tp1));
+    out("ClipLastPathname",tp2,hgl::filesystem::ClipLastPathname(tp2));
+    out("ClipLastPathname",tp3,hgl::filesystem::ClipLastPathname(tp3));
 
     return 0;
 }
