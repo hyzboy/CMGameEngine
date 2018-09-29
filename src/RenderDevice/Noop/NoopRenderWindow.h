4da14db42ea25f9c1cdbf14b4bc64611834d7726
hyzboy <hyzboy@gmail.com>
2018-09-29 17:26:01 +0800

    增加Noop设备的RenderWindow定义

---
+++ src/RenderDevice/Noop/NoopRenderWindow.h
@@ -0,0 +1,22 @@
+﻿#ifndef HGL_GRAPHICS_NOOP_RENDER_WINDOW_INCLUDE
+#define HGL_GRAPHICS_NOOP_RENDER_WINDOW_INCLUDE
+
+#include<hgl/graph/RenderWindow.h>
+namespace hgl
+{
+    namespace graph
+    {
+        /**
+         * 空设备渲染窗口
+         */
+        class NoopWindow:public RenderWindow
+        {
+        public:
+
+            using RenderWindow::RenderWindow;
+            virtual ~NoopWindow();
+
+        };//class NoopWindow:public RenderWindow
+    }//namespace graph
+}//namespace hgl
+#endif//HGL_GRAPHICS_NOOP_RENDER_WINDOW_INCLUDE
