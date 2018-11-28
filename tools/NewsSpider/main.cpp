30ca9346c72a4b5cfb7a78eb41091e590185a880
hyzboy <hyzboy@gmail.com>
2018-11-29 05:30:31 +0800

    定义统一的HTMLParse类，以统一部分代码

--- tools/NewsSpider/main.cpp
+++ tools/NewsSpider/main.cpp
@@ -12,10 +12,6 @@
 
 using namespace hgl;
 
-void ParseNewsPage(const char *url)
-{
-}
-
 const GumboAttribute *GetAttr(const GumboNode *node,const char *name)
 {
     for(int i=0;i<node->v.element.attributes.length;i++)
@@ -29,63 +25,97 @@ const GumboAttribute *GetAttr(const GumboNode *node,const char *name)
     return nullptr;
 }
 
+class HTMLParse
+{
+public:
+
+    HTMLParse()=default;
+    virtual ~HTMLParse()=default;
+
+    virtual void ParseNode(const GumboNode *node)=0;
+
+    virtual void Parse(const GumboNode *node)
+    {
+        ParseNode(node);
+
+        if(node->type!=GUMBO_NODE_ELEMENT)return;       //没有子节点
+
+        for(int i=0;i<node->v.element.children.length;i++)
+            Parse((const GumboNode *)(node->v.element.children.data[i]));
+    }
+public:
+
+    void Parse(const char *url)
+    {
+        io::MemoryOutputStream mos;
+
+        network::http::get(&mos,url);
+
+        GumboOutput *output=gumbo_parse_with_options(&kGumboDefaultOptions,(const char *)mos.GetData(),mos.Tell());
+
+        Parse(output->root);
+
+        gumbo_destroy_output(&kGumboDefaultOptions, output);
+    }
+};//class HTMLParse
+
+class SinaNewsPageParse
+{
+public:
+
+    void ParseNewsPage(const GumboNode *node)
+    {
+    }
+
+};//
+
 constexpr char NEWS_SINA_COM_CN_HEADER[]="https://news.sina.com.cn/";
 constexpr uint NEWS_SINA_COM_CN_HEADER_SIZE=sizeof(NEWS_SINA_COM_CN_HEADER)-1;
 constexpr char NEWS_LINK_END[]=".shtml";
 constexpr uint NEWS_LINK_END_SIZE=sizeof(NEWS_LINK_END)-1;
 
 /**
- * 节点查找
+ * 页内新闻链接查找
  * 范例: <a target="_blank" href="https://news.sina.com.cn/c/xl/2018-11-28/doc-ihpevhcm2060930.shtml">习近平出席西班牙国王举行的盛大欢迎仪式</a>
  * 判断依据: 为A点节，至少有两属性target,href，只有一个TEXT类型子节点
  *          target为"_blank"
  *          href前缀为"https://news.sina.com.cn"
  *          href结尾为".shtml"
  */
-void ParseNode(const GumboNode *node)
+class SinaNewsFirstPageParse:public HTMLParse
 {
-    if(node->type!=GUMBO_NODE_ELEMENT)return;
+public:
 
-    if(node->v.element.tag==GUMBO_TAG_A        //寻找A节点
-     &&node->v.element.attributes.length>=2    //至少2属性
-     &&node->v.element.children.length==1)     //只有一个子节点
+    void ParseNode(const GumboNode *node) override
     {
+        if(node->type!=GUMBO_NODE_ELEMENT)return;       //没有子节点
+        if(node->v.element.tag!=GUMBO_TAG_A)return;     //不是A节点
+        if(node->v.element.attributes.length<2)return;  //属性太少
+        if(node->v.element.children.length!=1)return;   //子节点数量不对
+
         const GumboAttribute *target=GetAttr(node,"target");
         const GumboAttribute *href=GetAttr(node,"href");
 
-        if(target&&href)
-        {
-            if(strcmp(target->value,"_blank")==0
-             &&strncmp(href->value,NEWS_SINA_COM_CN_HEADER,NEWS_SINA_COM_CN_HEADER_SIZE)==0)
-            {
-                const char *end_href=href->value+strlen(href->value)-NEWS_LINK_END_SIZE;
+        if(!target)return;
+        if(!href)return;
 
-                if(strcmp(end_href,NEWS_LINK_END)==0)
-                {
-                    const GumboNode *sub_node=(const GumboNode *)(node->v.element.children.data[0]);
+        if(strcmp(target->value,"_blank"))return;
+        if(strncmp(href->value,NEWS_SINA_COM_CN_HEADER,NEWS_SINA_COM_CN_HEADER_SIZE))return;
 
-                    if(sub_node->type==GUMBO_NODE_TEXT)     //是文本，确定它是新闻链接
-                    {
-                        std::cout<<"news: "<<sub_node->v.text.text<<std::endl;
-                        std::cout<<"link: "<<href->value<<std::endl;
-                    }
-                }
-            }
-        }
+        const char *end_href=href->value+strlen(href->value)-NEWS_LINK_END_SIZE;
+
+        if(strcmp(end_href,NEWS_LINK_END))return;
+
+        const GumboNode *sub_node=(const GumboNode *)(node->v.element.children.data[0]);
+
+        if(sub_node->type!=GUMBO_NODE_TEXT)return;
+
+        std::cout<<"news: "<<sub_node->v.text.text<<std::endl;
+        std::cout<<"link: "<<href->value<<std::endl;
+
+        //ParseNewsPage(href->value,sub_node->v.text.text);
     }
-
-    for(int i=0;i<node->v.element.children.length;i++)
-        ParseNode((const GumboNode *)(node->v.element.children.data[i]));
-}
-
-void ParseFirstPage(const void *data,const int size)
-{
-    GumboOutput *output=gumbo_parse_with_options(&kGumboDefaultOptions,(const char *)data,size);
-
-    ParseNode(output->root);
-
-    gumbo_destroy_output(&kGumboDefaultOptions, output);
-}
+};//
 
 HGL_CONSOLE_MAIN_FUNC()
 {
@@ -95,14 +125,8 @@ HGL_CONSOLE_MAIN_FUNC()
     if(!app.Init(&sii))
         return(-1);
 
-    UTF8String url="https://news.sina.com.cn";
+    SinaNewsFirstPageParse sina;
 
-    io::MemoryOutputStream mos;
-
-    network::http::get(&mos,url.c_str());
-
-    filesystem::SaveMemoryToFile(OS_TEXT("sina.html"),mos.GetData(),mos.Tell());
-
-    ParseFirstPage(mos.GetData(),mos.Tell());
+    sina.Parse("https://news.sina.com.cn");
     return 0;
 }
