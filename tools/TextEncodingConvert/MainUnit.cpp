e2deb6dce1616095607652d92ebaa1f31f9a3306
hyzboy <hyzboy@gmail.com>
2018-11-29 16:28:58 +0800

    更新TextEncodingConvert工具到最新的SDK

--- tools/TextEncodingConvert/MainUnit.cpp
+++ tools/TextEncodingConvert/MainUnit.cpp
@@ -1,6 +1,6 @@
-#include<hgl/Console.h>
+﻿#include<hgl/Console.h>
 #include<hgl/type/StringList.h>
-#include<hgl/filesystem/FileSystem.h>
+#include<hgl/filesystem/EnumFile.h>
 #include<hgl/io/FileOutputStream.h>
 #include<hgl/io/TextOutputStream.h>
 
@@ -59,217 +59,224 @@ void ConvertQtoB(u16char *str,int len)
     }
 }
 
-EnumFileConfig *ConvertSubFolderConfig(struct EnumFileConfig *efc,const OSString &sub_folder_name)
+class TECEnumFile:public EnumFile
 {
-    if(sub_folder_name.GetBeginChar()=='.')return(nullptr);         //跳过第一个字符为.的目录
+public:
 
-    for(int i=0;i<discard_folder_list.GetCount();i++)
+    EnumFileConfig *CreateSubConfig(struct EnumFileConfig *efc,const FileInfo &fi) override
     {
-        if(discard_folder_list[i].Comp(sub_folder_name)==0)
-            return(nullptr);
-    }
+        const OSString sub_folder_name=fi.name;
 
-    OSString old_path;
-    OSString new_path;
+        if(sub_folder_name.GetBeginChar()=='.')return(nullptr);         //跳过第一个字符为.的目录
 
-    old_path=MergeFilename(efc->folder_name,sub_folder_name);
-    new_path=MergeFilename(out_folder_base,old_path.c_str()+in_folder_base.Length());
-
-#if HGL_OS != HGL_OS_Windows
-    UTF16String u16_path=to_u16(new_path);
-
-    if(fn_english_fw2hw)
-    {
-        ConvertQtoB(u16_path.c_str(),u16_path.Length());
-
-        new_path=to_u8(u16_path);
-    }
-#else
-    ConvertQtoB(new_path.c_str(),new_path.Length());
-#endif//
-
-    if(!MakePath(new_path))
-    {
-        cmd_out<<OS_TEXT("[ERR] Create Directory failed: ")<<new_path.c_str()<<std::endl;
-        return(nullptr);
-    }
-    else
-    {
-        cmd_out<<OS_TEXT("[DIR] Create Directory: ")<<new_path.c_str()<<std::endl;
-    }
-
-    return DefaultCreateSubConfig(efc,sub_folder_name);
-}
-
-bool ExtNameCheck(const os_char *ext_name)
-{
-    const int len=strlen(ext_name);
-
-    for(int i=0;i<convert_ext_list.GetCount();i++)
-    {
-        const OSString &fn=convert_ext_list[i];
-
-        if(fn.Length()!=len)continue;
-
-        if(fn.CaseComp(ext_name)==0)
-            return(true);
-    }
-
-    return(false);
-}
-
-//未来优化内容：
-
-//  1.建立永久性的文件缓冲区，避每个文件都分配、释放内存。可提升性能.
-
-//  评估：现在已经很快了，不想做了。留给有心人做吧！
-
-template<typename TOS> void Convert(const OSString &in_filename,const OSString &out_filename)
-{
-    char *old_text;
-    union
-    {
-        char *u8_text;
-        u16char *u16_text;
-    }in_text;
-
-    const int64 old_size=LoadFileToMemory(in_filename,(void **)&old_text);
-    int64 in_size;
-
-    if(old_size<=0)return;
-
-    const BOMFileHeader *bom=ParseBOM(old_text);
-    CharSet in_cs;
-
-    if(bom)         //有BOM的
-    {
-        BOM2CharSet(&in_cs,bom);
-
-        if(in_cs==out_charset)                              //完全一样
+        for(int i=0;i<discard_folder_list.GetCount();i++)
         {
-            cmd_out<<OS_TEXT("[OK][COPY] filename: ")<<out_filename.c_str()<<std::endl;
+            if(discard_folder_list[i].Comp(sub_folder_name)==0)
+                return(nullptr);
+        }
 
-            SaveMemoryToFile(out_filename,old_text,old_size);        //直接保存
+        OSString old_path;
+        OSString new_path;
+
+        old_path=MergeFilename(efc->folder_name,sub_folder_name);
+        new_path=MergeFilename(out_folder_base,old_path.c_str()+in_folder_base.Length());
+
+    #if HGL_OS != HGL_OS_Windows
+        UTF16String u16_path=to_u16(new_path);
+
+        if(fn_english_fw2hw)
+        {
+            ConvertQtoB(u16_path.c_str(),u16_path.Length());
+
+            new_path=to_u8(u16_path);
+        }
+    #else
+        ConvertQtoB(new_path.c_str(),new_path.Length());
+    #endif//
+
+        if(!MakePath(new_path))
+        {
+            cmd_out<<OS_TEXT("[ERR] Create Directory failed: ")<<new_path.c_str()<<std::endl;
+            return(nullptr);
+        }
+        else
+        {
+            cmd_out<<OS_TEXT("[DIR] Create Directory: ")<<new_path.c_str()<<std::endl;
+        }
+
+        return EnumFile::CreateSubConfig(efc,fi);
+    }
+
+    bool ExtNameCheck(const os_char *ext_name)
+    {
+        const int len=strlen(ext_name);
+
+        for(int i=0;i<convert_ext_list.GetCount();i++)
+        {
+            const OSString &fn=convert_ext_list[i];
+
+            if(fn.Length()!=len)continue;
+
+            if(fn.CaseComp(ext_name)==0)
+                return(true);
+        }
+
+        return(false);
+    }
+
+    //未来优化内容：
+
+    //  1.建立永久性的文件缓冲区，避每个文件都分配、释放内存。可提升性能.
+
+    //  评估：现在已经很快了，不想做了。留给有心人做吧！
+
+    template<typename TOS> void Convert(const OSString &in_filename,const OSString &out_filename)
+    {
+        char *old_text;
+        union
+        {
+            char *u8_text;
+            u16char *u16_text;
+        }in_text;
+
+        const int64 old_size=LoadFileToMemory(in_filename,(void **)&old_text);
+        int64 in_size;
+
+        if(old_size<=0)return;
+
+        const BOMFileHeader *bom=ParseBOM(old_text);
+        CharSet in_cs;
+
+        if(bom)         //有BOM的
+        {
+            BOM2CharSet(&in_cs,bom);
+
+            if(in_cs==out_charset)                              //完全一样
+            {
+                cmd_out<<OS_TEXT("[OK][COPY] filename: ")<<out_filename.c_str()<<std::endl;
+
+                SaveMemoryToFile(out_filename,old_text,old_size);        //直接保存
+                delete[] old_text;
+                return;
+            }
+
+            in_text.u8_text=old_text+bom->size;
+            in_size=old_size-bom->size;
+        }
+        else
+        {
+            in_cs=in_charset;
+            in_text.u8_text=old_text;
+            in_size=old_size;
+        }
+
+        FileOutputStream fos;
+
+        if(!fos.CreateTrunc(out_filename))
+        {
             delete[] old_text;
+            cmd_out<<OS_TEXT("[ERR] Create file failed: ")<<out_filename.c_str()<<std::endl;
             return;
         }
 
-        in_text.u8_text=old_text+bom->size;
-        in_size=old_size-bom->size;
-    }
-    else
-    {
-        in_cs=in_charset;
-        in_text.u8_text=old_text;
-        in_size=old_size;
-    }
+        bool result=false;
+        TOS tos(&fos);
 
-    FileOutputStream fos;
+        if(include_bom)
+            tos.WriteBOM();
 
-    if(!fos.CreateTrunc(out_filename))
-    {
+        if(in_cs==out_charset)                                  // in/out一样，但走这里，代表它原本是没有BOM头的
+        {
+            result=tos.WriteChars(old_text,old_size);           //直接写入原始数据
+        }
+        else
+        if(in_cs==utf8_charset)
+        {
+            result=tos.WriteChars(in_text.u8_text,in_size);
+        }
+        else
+        if((in_cs==utf16be_charset&&out_charset==utf16le_charset)
+        ||(in_cs==utf16le_charset&&out_charset==utf16be_charset))      //16位互转
+        {
+            in_size>>=1;
+
+            EndianSwap<u16char>(in_text.u16_text,in_size);
+
+            result=tos.WriteChars(in_text.u16_text,in_size);
+        }
+        else
+        if(out_charset==utf8_charset)
+        {
+            char *new_text;
+            int new_size;
+
+            new_size=to_utf8(in_cs,&new_text,in_text.u8_text,in_size);
+
+            result=tos.WriteChars(new_text,new_size);
+
+            delete[] new_text;
+        }
+        else
+        if(out_charset==utf8_charset)
+        {
+            u16char *new_text;
+            int new_size;
+
+            new_size=to_utf16(in_cs,&new_text,in_text.u8_text,in_size);
+
+            result=tos.WriteChars(new_text,new_size);
+
+            delete[] new_text;
+        }
+
+        if(!result)
+            cmd_out<<OS_TEXT("[ERR] Write Text failed")<<std::endl;
+        else
+            cmd_out<<OS_TEXT("[OK] filename: ")<<out_filename.c_str()<<std::endl;
+
         delete[] old_text;
-        cmd_out<<OS_TEXT("[ERR] Create file failed: ")<<out_filename.c_str()<<std::endl;
-        return;
-    }
+    };
 
-    bool result=false;
-    TOS tos(&fos);
-
-    if(include_bom)
-        tos.WriteBOM();
-
-    if(in_cs==out_charset)                                  // in/out一样，但走这里，代表它原本是没有BOM头的
+    void ProcFile(struct EnumFileConfig *efc,FileInfo &fi) override
     {
-        result=tos.WriteChars(old_text,old_size);           //直接写入原始数据
+        if(fi.name[0]=='.')return;
+
+        for(int i=0;i<discard_file_list.GetCount();i++)
+        {
+            if(discard_file_list[i].Comp(fi.name)==0)
+                return;
+        }
+
+        const os_char *ext_name=strrchr<os_char>(fi.name,strlen(fi.name),'.');
+
+        if(!ext_name)return;
+
+        if(!ExtNameCheck(ext_name))return;
+
+        cmd_out<<OS_TEXT("[IN] filename: ")<<fi.fullname<<std::endl;
+
+        //生成新文件名
+
+        OSString new_filename=out_folder_base+(fi.fullname+in_folder_base.Length());
+
+        #if HGL_OS != HGL_OS_Windows
+        UTF16String u16_filename=to_u16(new_filename);
+
+        if(fn_english_fw2hw)
+        {
+            ConvertQtoB(u16_filename.c_str(),u16_filename.Length());
+
+            new_filename=to_u8(u16_filename);
+        }
+        #else
+        ConvertQtoB(new_filename.c_str(),new_filename.Length());
+        #endif//
+
+        if(out_charset.codepage==ccpUTF8    )Convert<UTF8TextOutputStream   >(fi.fullname,new_filename);else
+        if(out_charset.codepage==ccpUTF16LE )Convert<UTF16LETextOutputStream>(fi.fullname,new_filename);else
+        if(out_charset.codepage==ccpUTF16BE )Convert<UTF16BETextOutputStream>(fi.fullname,new_filename);
     }
-    else
-    if(in_cs==utf8_charset)
-    {
-        result=tos.WriteChars(in_text.u8_text,in_size);
-    }
-    else
-    if((in_cs==utf16be_charset&&out_charset==utf16le_charset)
-     ||(in_cs==utf16le_charset&&out_charset==utf16be_charset))      //16位互转
-    {
-        in_size>>=1;
-
-        EndianSwap<u16char>(in_text.u16_text,in_size);
-
-        result=tos.WriteChars(in_text.u16_text,in_size);
-    }
-    else
-    if(out_charset==utf8_charset)
-    {
-        char *new_text;
-        int new_size;
-
-        new_size=to_utf8(in_cs,&new_text,in_text.u8_text,in_size);
-
-        result=tos.WriteChars(new_text,new_size);
-
-        delete[] new_text;
-    }
-    else
-    if(out_charset==utf8_charset)
-    {
-        u16char *new_text;
-        int new_size;
-
-        new_size=to_utf16(in_cs,&new_text,in_text.u8_text,in_size);
-
-        result=tos.WriteChars(new_text,new_size);
-
-        delete[] new_text;
-    }
-
-    if(!result)
-        cmd_out<<OS_TEXT("[ERR] Write Text failed")<<std::endl;
-    else
-        cmd_out<<OS_TEXT("[OK] filename: ")<<out_filename.c_str()<<std::endl;
-
-    delete[] old_text;
-};
-
-void ConvertFile(struct EnumFileConfig *efc,FileInfo &fi)
-{
-    if(fi.name[0]=='.')return;
-
-    for(int i=0;i<discard_file_list.GetCount();i++)
-    {
-        if(discard_file_list[i].Comp(fi.name)==0)
-            return;
-    }
-
-    const os_char *ext_name=strrchr<os_char>(fi.name,'.');
-
-    if(!ext_name)return;
-
-    if(!ExtNameCheck(ext_name))return;
-
-    cmd_out<<OS_TEXT("[IN] filename: ")<<fi.fullname<<std::endl;
-
-    //生成新文件名
-
-    OSString new_filename=out_folder_base+(fi.fullname+in_folder_base.Length());
-
-    #if HGL_OS != HGL_OS_Windows
-    UTF16String u16_filename=to_u16(new_filename);
-
-    if(fn_english_fw2hw)
-    {
-        ConvertQtoB(u16_filename.c_str(),u16_filename.Length());
-
-        new_filename=to_u8(u16_filename);
-    }
-    #else
-    ConvertQtoB(new_filename.c_str(),new_filename.Length());
-    #endif//
-
-    if(out_charset.codepage==ccpUTF8    )Convert<UTF8TextOutputStream   >(fi.fullname,new_filename);else
-    if(out_charset.codepage==ccpUTF16LE )Convert<UTF16LETextOutputStream>(fi.fullname,new_filename);else
-    if(out_charset.codepage==ccpUTF16BE )Convert<UTF16BETextOutputStream>(fi.fullname,new_filename);
-}
+};//
 
 void out_string_list(const OSStringList &sl,const os_char *name)
 {
@@ -288,7 +295,7 @@ HGL_CONSOLE_MAIN_FUNC()
 {
     std::cout<<std::endl;
     std::cout<<"CMGDK Tools << Text Encoding Convert v2.0 >>"<<std::endl;
-    std::cout<<"(C)1997-2017 Copyright. Offical Web:www.hyzgame.com.cn"<<std::endl;
+    std::cout<<"(C)1997-2017 Copyright. Offical Web:www.hyzgame.com"<<std::endl;
     std::cout<<std::endl;
 
     LoadStringListFromTextFile(convert_ext_list,OS_TEXT("TextEncodingConvert.filter"),UTF8CharSet);
@@ -306,7 +313,7 @@ HGL_CONSOLE_MAIN_FUNC()
         std::cout<<"\t-sf\tproc sub-folder"<<std::endl;                                                     //处理子目录
         std::cout<<std::endl;
         std::cout<<" Format:  TextEncodingConvert [-fjh|-jh|-fbl|-bl|-no_bom|-sf] -in <us-ascii|shift_jis|big5|gbk|...> <input path> -out <utf8|utf16le> <output path>"<<std::endl;
-        std::cout<<" Example: TextEncodingConvert -in us-ascii -no_bom /home/hyzboy/input/ -out utf8 /home/hyzboy/output/"<<std::endl;
+        std::cout<<" Example: TextEncodingConvert -in us-ascii /home/hyzboy/input/ -out utf8 /home/hyzboy/output/"<<std::endl;
         std::cout<<std::endl;
 
         out_string_list(convert_ext_list,OS_TEXT("filter"));
@@ -416,15 +423,13 @@ HGL_CONSOLE_MAIN_FUNC()
 
     std::cout<<std::endl;
 
-    EnumFileConfig efc;
+    EnumFileConfig efc(in_folder_base);
 
-    efc.folder_name=in_folder_base;
     efc.sub_folder=sub_folder;
 
-    efc.CreateSubConfig=ConvertSubFolderConfig;
-    efc.cb_file=ConvertFile;
+    TECEnumFile tef;
 
-    EnumFile(&efc);
+    tef.Enum(&efc);
 
     return 0;
 }
