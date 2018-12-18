4dd2b215d73390999cfff795fb2cbbe112fa8de2
hyzboy <hyzboy@gmail.com>
2018-12-18 21:39:03 +0800

    增加今日头条抓取(仅首页，似乎有反抓取)

--- tools/NewsSpider/ToutiaoNews.cpp
+++ tools/NewsSpider/ToutiaoNews.cpp
@@ -1,4 +1,5 @@
 ﻿#include<hgl/Console.h>
+#include<hgl/filesystem/FileSystem.h>
 #include<hgl/network/HTTPTools.h>
 #include<hgl/io/MemoryOutputStream.h>
 #include<hgl/io/FileOutputStream.h>
@@ -8,9 +9,9 @@
 #include<hgl/webapi/UserAgentString.h>
 
 #if HGL_OS == HGL_OS_Windows
-#include<gumbo.h>      
+#include<gumbo.h>
 #else
-#include<gumbo-parser/gumbo.h> 
+#include<gumbo-parser/gumbo.h>
 #endif//
 
 #include<iostream>
@@ -20,30 +21,124 @@ using namespace hgl::io;
 using namespace hgl::filesystem;
 using namespace hgl::webapi;
 
-HGL_CONSOLE_MAIN_FUNC()
+UTF8String user_agent;
+
+void InitUserAgent()
 {
     FirefoxUserAgentConfig cfg;
 
-    cfg.os              =OS_iPhone;
+    cfg.os              =OS_iPad;
 
     cfg.os_ver.major    =12;
     cfg.os_ver.minor    =1;
 
-    cfg.ff_ver.major    =63;
+    cfg.ff_ver.major    =64;
     cfg.ff_ver.minor    =0;
 
-    UTF8String user_agent=FirefoxUserAgent(cfg);
-
-    std::cout<<"iPhone: "<<user_agent.c_str()<<std::endl;
-
-    cfg.os=OS_iPad;
     user_agent=FirefoxUserAgent(cfg);
-    std::cout<<"iPad: "<<user_agent.c_str()<<std::endl;
+}
 
-    cfg.os=OS_AndroidPhone;
-    cfg.os_ver.major=9;
-    user_agent=FirefoxUserAgent(cfg);
-    std::cout<<"Android 9: "<<user_agent.c_str()<<std::endl;
+class HTMLParse
+{
+private:
+
+    UTF8String web_url;
+    MemoryOutputStream mos;
+
+    CharSet page_charset;
+
+protected:
+
+    const UTF8String GetSubText(const GumboNode *node,int index)
+    {
+        if(index<0||index>=node->v.element.children.length)
+            return(nullptr);
+
+        const GumboNode *sub_node=(const GumboNode *)(node->v.element.children.data[index]);
+
+        if(sub_node->type!=GUMBO_NODE_TEXT)
+            return(nullptr);
+
+        if(page_charset.codepage==ccpUTF8)
+            return UTF8String(sub_node->v.text.text);
+        else
+        {
+            char *u8str;
+
+            int len=to_utf8(page_charset,
+                            &u8str,
+                            sub_node->v.text.original_text.data,
+                            sub_node->v.text.original_text.length);
+
+            return UTF8String(u8str,len,true);
+        }
+    }
+
+public:
+
+    HTMLParse()
+    {
+        page_charset=CharSet("utf-8");
+    }
+
+    virtual ~HTMLParse()=default;
+
+    virtual void ParseNode(const GumboNode *node)=0;
+
+    virtual void Parse(const GumboNode *node)
+    {
+        if(node->type!=GUMBO_NODE_ELEMENT)return;       //没有子节点
+
+        ParseNode(node);
+
+        for(int i=0;i<node->v.element.children.length;i++)
+            Parse((const GumboNode *)(node->v.element.children.data[i]));
+    }
+
+public:
+
+    void Parse(const UTF8String &url)
+    {
+        web_url=url;
+        network::http::get(&mos,url.c_str());
+
+        SaveMemoryToFile("FirstPage.html",mos.GetData(),mos.Tell());
+
+        GumboOutput *output=gumbo_parse_with_options(&kGumboDefaultOptions,(const char *)mos.GetData(),mos.Tell());
+
+        Parse(output->root);
+
+        gumbo_destroy_output(&kGumboDefaultOptions, output);
+    }
+};//class HTMLParse
+
+class FirstPageParse:public HTMLParse
+{
+public:
+
+    using HTMLParse::HTMLParse;
+    ~FirstPageParse()=default;
+
+    void ParseNode(const GumboNode *node) override
+    {
+    }
+};//class FirstPageParse:public HTMLParse
+
+HGL_CONSOLE_MAIN_FUNC()
+{
+    if(args.GetCount()<2)
+    {
+        std::cerr<<"use: ToutianNews name"<<std::endl;
+        return 0;
+    }
+
+    InitUserAgent();
+
+    FirstPageParse fpp;
+
+    const UTF8String url="https://m.toutiao.com/search/?keyword="+args[1];
+
+    fpp.Parse(url);
 
     return 0;
 }
