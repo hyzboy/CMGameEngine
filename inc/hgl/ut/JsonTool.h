83054b810cec6473eeea4ac58c909d817463e61b
hyzboy <hyzboy@gmail.com>
2018-12-19 18:39:16 +0800

    jsontool增加名字空间

--- inc/hgl/ut/JsonTool.h
+++ inc/hgl/ut/JsonTool.h
@@ -3,9 +3,12 @@
 #include<json/json.h>
 #include<hgl/type/BaseString.h>
 
-const std::string JsonToString(const Json::Value &jv_root);
+namespace hgl
+{
+    const std::string JsonToString(const Json::Value &jv_root);
 
-bool ParseJson(Json::Value &root,const char *str,const int size,std::string *error_info);
+    bool ParseJson(Json::Value &root,const char *str,const int size,std::string *error_info);
 
-bool LoadJson(Json::Value &,const hgl::OSString &filename);
-bool SaveJson(Json::Value &,const hgl::OSString &filename);
+    bool LoadJson(Json::Value &,const hgl::OSString &filename);
+    bool SaveJson(Json::Value &,const hgl::OSString &filename);
+};//
