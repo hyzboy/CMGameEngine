7fdfa9faef395b73f4b8dde04c6b2a134dcb0189
hyzboy <hyzboy@gmail.com>
2019-05-17 20:39:51 +0800

    模块改进

--- inc/hgl/io/MemoryOutputStream.h
+++ inc/hgl/io/MemoryOutputStream.h
@@ -5,48 +5,48 @@
 #include<hgl/io/OutputStream.h>
 namespace hgl
 {
-	namespace io
-	{
-		/**
-		 * 内存数据输出流，将数据输出到一个内存块中。
-		 */
-		class MemoryOutputStream:public OutputStream												///内存数据输出流
-		{
-		protected:
+    namespace io
+    {
+        /**
+         * 内存数据输出流，将数据输出到一个内存块中。
+         */
+        class MemoryOutputStream:public OutputStream                                                ///内存数据输出流
+        {
+        protected:
 
-			uint8 *buf;
+            uint8 *buf;
 
-			size_t max_size;
-			size_t buf_size;
-			size_t cur_pos;
+            size_t max_size;
+            size_t buf_size;
+            size_t cur_pos;
 
-			bool one;
+            bool one;
 
-		public:
+        public:
 
-			MemoryOutputStream()
-			{
-				buf=0;
-				max_size=0;
-				buf_size=0;
-				cur_pos=0;
-				one=false;
-			}
+            MemoryOutputStream()
+            {
+                buf=0;
+                max_size=0;
+                buf_size=0;
+                cur_pos=0;
+                one=false;
+            }
 
-			virtual ~MemoryOutputStream()
-			{
-				Close();
-			}
+            virtual ~MemoryOutputStream()
+            {
+                Close();
+            }
 
-			void *GetData()const{return buf;}
+            void *GetData()const{return buf;}
 
-			/**
+            /**
              * 创建一个当前流数据的内存拷贝，此函数需用户自行delete[]
              * @param len 存放数据长度的指针
              * @return 创建好的内存拷贝
              */
-			void *CreateCopyData(int *len)const
-			{
+            void *CreateCopyData(uint *len)const
+            {
                 if(buf_size<=0)
                     return(nullptr);
 
@@ -60,177 +60,177 @@ namespace hgl
                 return data;
             }
 
-			/**
-			 * 关联一个数据区到当前输出流
-			 * @param ptr 数据指针
-			 * @param size 数据长度字节数
-			 * @param one_instance 是否仅此一份实例(如果是，将由MemoryOutputStream类负责释放)
-			 * @return 是否成功
-			 */
-			bool Link(void *ptr,size_t size,bool one_instance=false)
-			{
-				if(!ptr||!size)
-					return(false);
+            /**
+             * 关联一个数据区到当前输出流
+             * @param ptr 数据指针
+             * @param size 数据长度字节数
+             * @param one_instance 是否仅此一份实例(如果是，将由MemoryOutputStream类负责释放)
+             * @return 是否成功
+             */
+            bool Link(void *ptr,size_t size,bool one_instance=false)
+            {
+                if(!ptr||!size)
+                    return(false);
 
-				buf=(uint8 *)ptr;
-				buf_size=size;
-				max_size=size;
-				cur_pos=0;
+                buf=(uint8 *)ptr;
+                buf_size=size;
+                max_size=size;
+                cur_pos=0;
 
-				one=one_instance;
+                one=one_instance;
 
-				return(true);
-			}
+                return(true);
+            }
 
-			/**
-			 * 更新关联的数据区长度，不复位cur_pos
-			 * @param ptr 数据指针
-			 * @param size 数据长度字节数
-			 * @return 是否成功
-			 */
-			bool Update(void *ptr,size_t size)
-			{
-				if(!ptr||!size)
-					return(false);
+            /**
+             * 更新关联的数据区长度，不复位cur_pos
+             * @param ptr 数据指针
+             * @param size 数据长度字节数
+             * @return 是否成功
+             */
+            bool Update(void *ptr,size_t size)
+            {
+                if(!ptr||!size)
+                    return(false);
 
-				buf=(uint8 *)ptr;
-				buf_size=size;
-				max_size=0;
+                buf=(uint8 *)ptr;
+                buf_size=size;
+                max_size=0;
 
-				return(true);
-			}
+                return(true);
+            }
 
-			void Unlink()
-			{
-				buf=0;
-				buf_size=0;
-				max_size=0;
-			}
+            void Unlink()
+            {
+                buf=0;
+                buf_size=0;
+                max_size=0;
+            }
 
-			bool Create(int64 size)
-			{
-				Close();
+            bool Create(int64 size)
+            {
+                Close();
 
-				if(size<=0)
-					return(false);
+                if(size<=0)
+                    return(false);
 
-				buf=(uint8 *)hgl_malloc(size);
+                buf=(uint8 *)hgl_malloc(size);
 
-				if(!buf)
-					return(false);
+                if(!buf)
+                    return(false);
 
-				one=true;
-				cur_pos=0;
-				buf_size=size;
-				max_size=size;
+                one=true;
+                cur_pos=0;
+                buf_size=size;
+                max_size=size;
 
-				return(true);
-			}
+                return(true);
+            }
 
-			void Close() override
-			{
-				if(buf)
-				{
-					if(one)
-						hgl_free(buf);
+            void Close() override
+            {
+                if(buf)
+                {
+                    if(one)
+                        hgl_free(buf);
 
-					buf=0;
-				}
+                    buf=0;
+                }
 
-				buf_size=0;
-				max_size=0;
-			}
+                buf_size=0;
+                max_size=0;
+            }
 
-			void ClearData()
-			{
-				cur_pos=0;
-				buf_size=0;
-			}
+            void ClearData()
+            {
+                cur_pos=0;
+                buf_size=0;
+            }
 
-			int64 Write(const void *ptr,int64 size) override
-			{
-				if(!ptr||size<0)
-					return(-1);
+            int64 Write(const void *ptr,int64 size) override
+            {
+                if(!ptr||size<0)
+                    return(-1);
 
-				if(!buf)
-				{
-					if(!Create(size))
-						return(-1);
-				}
+                if(!buf)
+                {
+                    if(!Create(size))
+                        return(-1);
+                }
 
-				if(cur_pos+size>buf_size)
-				{
-					if(one)
-					{
-						buf_size=cur_pos+size;
+                if(cur_pos+size>buf_size)
+                {
+                    if(one)
+                    {
+                        buf_size=cur_pos+size;
 
-						if(buf_size>max_size)
-						{
-							max_size=power_to_2(buf_size);
+                        if(buf_size>max_size)
+                        {
+                            max_size=power_to_2(buf_size);
 
-							buf=(uint8 *)hgl_realloc(buf,max_size);
-						}
-					}
-					else
-						size=buf_size-cur_pos;
-				}
+                            buf=(uint8 *)hgl_realloc(buf,max_size);
+                        }
+                    }
+                    else
+                        size=buf_size-cur_pos;
+                }
 
-				if(size<=0)
-					return(0);
+                if(size<=0)
+                    return(0);
 
-				memcpy(buf+cur_pos,ptr,size);
+                memcpy(buf+cur_pos,ptr,size);
 
-				cur_pos+=size;
+                cur_pos+=size;
 
-				return size;
-			}
+                return size;
+            }
 
-			bool	CanRestart()const override{return true;}
-			bool	CanSeek()const override{return true;}
-			bool	CanSize()const override{return true;}
+            bool    CanRestart()const override{return true;}
+            bool    CanSeek()const override{return true;}
+            bool    CanSize()const override{return true;}
 
-			bool	Restart() override
-			{
-				cur_pos=0;
-				return(true);
-			}
+            bool    Restart() override
+            {
+                cur_pos=0;
+                return(true);
+            }
 
-			int64	Seek(int64 off,SeekOrigin so=soBegin) override
-			{
-				if(!CanSeek())return(-1);
+            int64   Seek(int64 off,SeekOrigin so=soBegin) override
+            {
+                if(!CanSeek())return(-1);
 
-				if(so==soCurrent)
-				{
-					off+=cur_pos;
-				}
-				else
-				if(so==soEnd)
-				{
-					off+=buf_size;
-				}
+                if(so==soCurrent)
+                {
+                    off+=cur_pos;
+                }
+                else
+                if(so==soEnd)
+                {
+                    off+=buf_size;
+                }
 
-				if(off<0||off>=buf_size)
-					return(-1);
+                if(off<0||off>=buf_size)
+                    return(-1);
 
-				cur_pos=off;
-				return cur_pos;
-			}
+                cur_pos=off;
+                return cur_pos;
+            }
 
-			int64	Tell()const override
-			{
-				return cur_pos;
-			}
+            int64   Tell()const override
+            {
+                return cur_pos;
+            }
 
-			int64	GetSize()const override
-			{
-				return buf_size;
-			}
+            int64   GetSize()const override
+            {
+                return buf_size;
+            }
 
-			int64	Available()const override
-			{
-				return buf_size-cur_pos;
-			}
-		};//class MemoryOutputStream
-	}//namespace io
+            int64   Available()const override
+            {
+                return buf_size-cur_pos;
+            }
+        };//class MemoryOutputStream
+    }//namespace io
 }//namespace hgl
 #endif//HGL_IO_MEMORY_OUTPUT_STREAM_INCLUDE
--- inc/hgl/type/List.h
+++ inc/hgl/type/List.h
@@ -1,80 +1,80 @@
-#ifndef HGL_LIST_INCLUDE
+﻿#ifndef HGL_LIST_INCLUDE
 #define HGL_LIST_INCLUDE
 
-#include<hgl/LogInfo.h>
 #include<stdlib.h>
 #include<initializer_list>
-
-#if defined(__BORLANDC__)||defined(__TURBOC__)||defined(__CODEGEARC__)
-#pragma warn -8022						//使hides vitual function警告无效
-#endif//
+#include<hgl/type/DataType.h>
 
 namespace hgl
 {
-	/**
-	* List类用于保存数据列表。可以在列表中添加、删除、查找、访问和排序数据。
-	*/
+    /**
+    * List类用于保存数据列表。可以在列表中添加、删除、查找、访问和排序数据。
+    */
     template <typename T> class List                                                                ///列表处理类
-	{
-	protected:
+    {
+    protected:
 
-		int count;
-		int max_count;
-		T *items;
+        int count=0;
+        int max_count=0;
+        T *items=nullptr;
 
-	public:	//属性
+    public: //属性
 
-						int		GetCount	()const{return count;}									///<取得列表内数据数量
-		virtual			void	SetCount	(int);													///<设置列表内数据数量
-		virtual			void	PreMalloc	(int);													///<预分配指定数量的数据空间
-						T *		GetData		()const{return items;}									///<提供原始数据项
-						int		GetBytes	()const{return count*sizeof(T);}						///<取得原始数据总字节数
+                        int     GetCount    ()const{return count;}                                  ///<取得列表内数据数量
+        virtual         void    SetCount    (int);                                                  ///<设置列表内数据数量
+        virtual         void    PreMalloc   (int);                                                  ///<预分配指定数量的数据空间
+                        T *     GetData     ()const{return items;}                                  ///<提供原始数据项
+                        int     GetBytes    ()const{return count*sizeof(T);}                        ///<取得原始数据总字节数
 
-	public: //方法
+    public: //方法
 
-		List();                                                                                     ///<本类构造函数
-		List(const List<T> &lt){operator=(lt);}														///<本类构造函数
-		List(const std::initializer_list<T> &lt){operator=(lt);}
+        List(){};                                                                                   ///<本类构造函数
+        List(const List<T> &lt){operator=(lt);}                                                     ///<本类构造函数
+        List(const std::initializer_list<T> &lt){operator=(lt);}
 
-		virtual ~List(){Clear();}                                                                   ///<本类析构函数
+        virtual ~List(){Clear();}                                                                   ///<本类析构函数
 
-		virtual T *  Add();																			///<添加一个空数据
-		virtual int  Add(const T &);																///<增加一个数据
-		virtual int  Add(const T &,int n);															///<重复增加一个数据
-		virtual int  Add(const T *,int n);															///<增加一批数据
-				int  Add(const List<T> &l){return Add(l.items,l.count);}							///<增加一批数据
+        virtual T *  Add();                                                                         ///<添加一个空数据
+        virtual int  Add(const T &);                                                                ///<增加一个数据
+        virtual int  Add(const T &,int n);                                                          ///<重复增加一个数据
+        virtual int  Add(const T *,int n);                                                          ///<增加一批数据
+                int  Add(const List<T> &l){return Add(l.items,l.count);}                            ///<增加一批数据
 
-		virtual void Clear();																		///<清除所有数据
-		virtual void ClearData();																	///<清除所有数据，但不清空缓冲区
-		virtual int  Find(const T &)const;															///<查找指定数据的索引
+        virtual void Clear();                                                                       ///<清除所有数据
+        virtual void ClearData();                                                                   ///<清除所有数据，但不清空缓冲区
+        virtual int  Find(const T &)const;                                                          ///<查找指定数据的索引
                 bool IsExist(const T &flag)const{return Find(flag)!=-1;}                            ///<确认数据项是否存在
-		virtual bool Delete(int);																	///<删除指定索引的数据
-		virtual bool Delete(int,int);																///<删除指定索引的数据
-		virtual bool DeleteMove(int);																///<删除指定索引的数据,将后面紧邻的数据前移
-		virtual	bool DeleteByValue(const T &);														///<删除一个指定数据
-		virtual void DeleteByValue(const T *,int);													///<删除一批指定的数据
-		virtual	void Exchange(int,int);																///<根据索引交换两个数据
-		virtual void Insert(int,const T &);															///<在指定索引处插入一个数据
-		virtual	void Move(int,int);																	///<移动一个数据到移指索引处
+        virtual bool Delete(int);                                                                   ///<删除指定索引的数据
+        virtual bool Delete(int,int);                                                               ///<删除指定索引的数据
+        virtual bool DeleteMove(int);                                                               ///<删除指定索引的数据,将后面紧邻的数据前移
+        virtual bool DeleteByValue(const T &);                                                      ///<删除一个指定数据
+        virtual void DeleteByValue(const T *,int);                                                  ///<删除一批指定的数据
+        virtual void Exchange(int,int);                                                             ///<根据索引交换两个数据
+        virtual void Insert(int,const T &);                                                         ///<在指定索引处插入一个数据
+        virtual void Move(int,int);                                                                 ///<移动一个数据到移指索引处
 
-            	void DeleteClear();																	///<清除所有数据并全部调用delete
+                void DeleteClear();                                                                 ///<清除所有数据并全部调用delete
 
-		virtual void operator = (const List<T> &);													///<操作符重载复制一个列表
-		virtual void operator = (const std::initializer_list<T> &l);
+        virtual void operator = (const List<T> &);                                                  ///<操作符重载复制一个列表
+        virtual void operator = (const std::initializer_list<T> &l);
 
-		virtual void operator += (const T &obj){Add(obj);}											///<操作符重载添加一个数据
-		virtual void operator << (const T &obj){Add(obj);}											///<操作符重载添加一个数据
-		virtual void operator -= (const T &obj){DeleteByValue(obj);}									///<操作符重载删除一个数据
+        virtual void operator += (const T &obj){Add(obj);}                                          ///<操作符重载添加一个数据
+        virtual void operator << (const T &obj){Add(obj);}                                          ///<操作符重载添加一个数据
+        virtual void operator -= (const T &obj){DeleteByValue(obj);}                                ///<操作符重载删除一个数据
 
-				bool Get(int,T &)const;																///<取得指定索引处的数据
-				void Set(int,const T &);															///<设置指定索引处的数据
-				bool Rand(T &)const;																///<随机取得一个数据
+                T *  GetPointer(const int index)                                                        ///<取得指定序列号数据的索引
+                {
+                    return(index>=count?nullptr:items+index);
+                }
 
-		virtual T &operator[](int n)const;															///<操作符重载取得指定索引处的数据
-		virtual bool Begin(T &)const;																///<取第一个数据
-		virtual bool End(T &)const;																	///<取最后一个数据
+                bool Get(int,T &)const;                                                             ///<取得指定索引处的数据
+                void Set(int,const T &);                                                            ///<设置指定索引处的数据
+                bool Rand(T &)const;                                                                ///<随机取得一个数据
 
-		virtual void Enum(void (*enum_func)(T &))                                                   ///<枚举所有数据成员
+        virtual bool Begin(T &)const;                                                               ///<取第一个数据
+        virtual bool End(T &)const;                                                                 ///<取最后一个数据
+
+        virtual void Enum(void (*enum_func)(T &))                                                   ///<枚举所有数据成员
         {
             T *obj=items;
 
@@ -84,75 +84,77 @@ namespace hgl
                 ++obj;
             }
         }
-	};//template <typename T> class List
+    };//template <typename T> class List
 
-	template<typename T> T *GetObject(const List<T *> &list,const int index)
-	{
-		T *obj;
+    template<typename T> T *GetObject(const List<T *> &list,const int index)
+    {
+        T *obj;
 
-		if(list.Get(index,obj))
-			return(obj);
+        if(list.Get(index,obj))
+            return(obj);
 
-		return(nullptr);
-	}
+        return(nullptr);
+    }
 }//namespace hgl
 
 #include<hgl/type/List.cpp>
 //--------------------------------------------------------------------------------------------------
 namespace hgl
 {
-	/**
-	* 自定义对象列表处理类与标准列表处理类的区别在于它对数据生成/清除时会多调用虚拟函数Create/Delte
-	*/
-	template<typename T> class CusObjectList:public List<T *>										///对象列表处理类
-	{
-		virtual void	SetCount(int);
+    /**
+    * 自定义对象列表处理类与标准列表处理类的区别在于它对数据生成/清除时会多调用虚拟函数Create/Delte
+    */
+    template<typename T> class CusObjectList:public List<T *>                                       ///对象列表处理类
+    {
+    public:
 
-//		virtual T *		CreateObject()=0;															///<创建一个数据，但不加入列表
-		virtual void	DeleteObject(T *)=0;														///<删除一个数据
+        virtual void    SetCount(int);
 
-	public:
+//      virtual T *     CreateObject()=0;                                                           ///<创建一个数据，但不加入列表
+        virtual void    DeleteObject(T *)=0;                                                        ///<删除一个数据
 
-		typedef T *ItemPointer;
+    public:
 
-	public: //方法
+        typedef T *ItemPointer;
+
+    public: //方法
 
         CusObjectList(){}
-		virtual ~CusObjectList();
+        virtual ~CusObjectList();
 
-	public:
+    public:
 
-//		virtual T *		Append();																	///<追加一个数据
-//		virtual T *		Insert(int);																///<在指定索引处创建一个数据
-				void	Insert(int,const ItemPointer &);											///<在指定索引处插入一个数据
+//      virtual T *     Append();                                                                   ///<追加一个数据
+//      virtual T *     Insert(int);                                                                ///<在指定索引处创建一个数据
+                void    Insert(int,const ItemPointer &);                                            ///<在指定索引处插入一个数据
 
-		virtual void	Clear();																	///<清除所有数据
-		virtual void	ClearData();																///<清除所有数据，但不清空缓冲区
+        virtual void    Clear();                                                                    ///<清除所有数据
+        virtual void    ClearData();                                                                ///<清除所有数据，但不清空缓冲区
 
-		virtual bool	Unlink(int index){return List<T *>::Delete(index);}							///<将指定索引处的数据与列表断开
-		virtual bool	UnlinkMove(int index){return List<T *>::DeleteMove(index);}					///<将指定索引处的数据与列表断开,将前移后面的数据
-		virtual bool	Unlink(int start,int number){return List<T *>::Delete(start,number);}		///<将指定索引处的数据与列表断开
-		virtual bool	UnlinkByValue(const ItemPointer &ip){return List<T *>::DeleteByValue(ip);}	///<将一个指定数据与列表断开
-		virtual void	UnlinkByValue(const ItemPointer *ip,int n){List<T *>::DeleteByValue(ip,n);}	///<将一批指定数据与列表断开
-		virtual void	UnlinkAll(){List<T *>::ClearData();}										///<断开所有数据
+        virtual bool    Unlink(int index){return List<T *>::Delete(index);}                         ///<将指定索引处的数据与列表断开
+        virtual bool    UnlinkMove(int index){return List<T *>::DeleteMove(index);}                 ///<将指定索引处的数据与列表断开,将前移后面的数据
+        virtual bool    Unlink(int start,int number){return List<T *>::Delete(start,number);}       ///<将指定索引处的数据与列表断开
+        virtual bool    UnlinkByValue(const ItemPointer &ip){return List<T *>::DeleteByValue(ip);}  ///<将一个指定数据与列表断开
+        virtual void    UnlinkByValue(const ItemPointer *ip,int n){List<T *>::DeleteByValue(ip,n);} ///<将一批指定数据与列表断开
+        virtual void    UnlinkAll(){List<T *>::ClearData();}                                        ///<断开所有数据
 
-		virtual bool	Delete(int);																///<删除指定索引处的数据
-		virtual bool	DeleteMove(int);															///<删除指定索引处的数据
-		virtual bool	DeleteByValue(const ItemPointer &);											///<删除指定的一个数据
-		virtual void	DeleteByValue(const ItemPointer *,int);										///<删除指定的一批数据
-		virtual void	DeleteAll();																///<删除所有数据
+        virtual bool    Delete(int);                                                                ///<删除指定索引处的数据
+        virtual bool    DeleteMove(int);                                                            ///<删除指定索引处的数据
+        virtual bool    DeleteByValue(const ItemPointer &);                                         ///<删除指定的一个数据
+        virtual void    DeleteByValue(const ItemPointer *,int);                                     ///<删除指定的一批数据
+        virtual void    DeleteAll();                                                                ///<删除所有数据
 
-		virtual ItemPointer &operator[](int n)const													///<操作符重载取得指定索引处的数据
-		{
-			static T *null_pointer=nullptr;
+        virtual ItemPointer &operator[](int n)const                                                 ///<操作符重载取得指定索引处的数据
+        {
+            static T *null_pointer=nullptr;
 
-			if(n<0||n>=this->count)
-				return(null_pointer);
+            if(n<0||n>=this->count)
+                return(null_pointer);
 
-			return this->items[n];
-		}
+            return this->items[n];
+        }
 
-		virtual void Enum(void (*enum_func)(T *))                                                     ///<枚举所有数据成员
+        virtual void Enum(void (*enum_func)(T *))                                                     ///<枚举所有数据成员
         {
             T **obj=this->items;
 
@@ -163,26 +165,26 @@ namespace hgl
                 ++obj;
             }
         }
-	};//template <typename T> class CusObjectList
+    };//template <typename T> class CusObjectList
 
-	/**
-	* 对象列表处理类<br>
-	* 将自定义对象列表中的Create重载为new,Delete重载为delete
-	*/
-	template<typename T> class ObjectList:public CusObjectList<T>
-	{
-	private:
+    /**
+    * 对象列表处理类<br>
+    * 将自定义对象列表中的Create重载为new,Delete重载为delete
+    */
+    template<typename T> class ObjectList:public CusObjectList<T>
+    {
+    private:
 
-//		virtual T *		CreateObject(){return(new T);}												///<创建一个数据
-		virtual void	DeleteObject(T *obj){if(obj)delete obj;}                                    ///<删除一个数据
+//      virtual T *     CreateObject(){return(new T);}                                              ///<创建一个数据
+        virtual void    DeleteObject(T *obj){if(obj)delete obj;}                                    ///<删除一个数据
 
-	public:
+    public:
 
-		virtual ~ObjectList()
-		{
-			CusObjectList<T>::Clear();
-		}
-	};//class ObjectList
+        virtual ~ObjectList()
+        {
+            CusObjectList<T>::Clear();
+        }
+    };//class ObjectList
 }//namespace hgl
 #include<hgl/type/ObjectList.cpp>
 #endif//HGL_LIST_INCLUDE
